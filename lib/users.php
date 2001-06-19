3d22f26da596ad582b2662a0307c43b205d5cbc5
Shmuel Melamud <balu@migdal.ru>
2001-06-19 10:35:28 +0000

    User list

--- lib/users.php
+++ lib/users.php
@@ -2,6 +2,7 @@
 # @(#) $Id$
 
 require_once('lib/dataobject.php');
+require_once('lib/selectiterator.php');
 
 class User
       extends DataObject
@@ -22,6 +23,69 @@ var $email_disabled;
 function User($row)
 {
 $this->DataObject($row);
+}
+
+function getId()
+{
+return $this->id;
+}
+
+function getLogin()
+{
+return $this->login;
+}
+
+function getFullName()
+{
+return $this->name.($this->jewish_name!='' ? ' ('.$this->jewish_name.')' : '').
+       ' '.$this->surname;
+}
+
+function getAge()
+{
+$bt=explode('-',$this->birthday);
+$t=getdate();
+$jbt=GregorianToJD($bt[1],$bt[2],$bt[0]);
+$jt=GregorianToJD($t['mon'],$t['mday'],$t['year']);
+return (int)(($jt-$jbt)/365.25);
+}
+
+function getMigdalStudent()
+{
+return $this->migdal_student ? 'Да' : 'Нет';
+}
+
+function getEmailLink()
+{
+return $this->email!=''
+       ? '<a href="mailto:'.$this->email.'">'.$this->email.'</a>'
+       : '';
+}
+
+function getICQ()
+{
+return $this->icq;
+}
+
+function getLastOnline()
+{
+$t=strtotime($this->last_online);
+return date('j/m/Y в H:i:s',$t);
+}
+
+}
+
+class UserListIterator
+      extends SelectIterator
+{
+
+function UserListIterator()
+{
+$this->SelectIterator('User',
+                      'select id,login,name,jewish_name,surname,birthday,
+		              migdal_student,email,icq,last_online
+		       from users
+		       order by login');
 }
 
 }
--- users.php
+++ users.php
@@ -15,6 +15,32 @@ require_once('top.php');
   <?php
   dbOpen();
   displayTop('users');
+  ?>
+  <p>
+  <a href="useredit.php">Зарегистрироваться</a><br>
+  <table>
+   <tr valign=top>
+    <th>Ник</th><th>Имя</th><th>Возраст</th><th>Мигдалевец</th><th>E-mail</th>
+    <th>ICQ</th><th>Последний раз<br>был здесь</th>
+   </tr>
+   <?php
+   $list=new UserListIterator();
+   while($item=$list->next())
+        {
+	echo '<tr>';
+	echo '<td align=center><a href="useredit.php?editid='.$item->getId().
+	     '">'.$item->getLogin().'</a></td>';
+	echo '<td align=center>'.$item->getFullName().'</td>';
+	echo '<td align=center>'.$item->getAge().'</td>';
+	echo '<td align=center>'.$item->getMigdalStudent().'</td>';
+	echo '<td align=center>'.$item->getEmailLink().'</td>';
+	echo '<td align=center>'.$item->getICQ().'</td>';
+	echo '<td align=center>'.$item->getLastOnline().'</td>';
+	echo '</tr>';
+	}
+   ?>
+  </table>
+  <?php
   dbClose();
   ?>
 </body>
