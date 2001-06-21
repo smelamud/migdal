1c359f5ff6224442aa2ff0f7c89cad4eb590ea0f
Shmuel Melamud <balu@migdal.ru>
2001-06-21 21:56:44 +0000

    Leave link to current active page in menu

--- top.php
+++ top.php
@@ -12,8 +12,9 @@ $menu=new MenuIterator($current);
 while($item=$menu->next())
      {
      $s='['.$item->getName().']';
-     echo $item->isCurrent() ? "<b>$s</b>"
-                             : '<a href="'.$item->getLink()."\">$s</a>";
+     echo '<a href="'.$item->getLink().'">'.
+            ($item->isCurrent() ? "<b>$s</b>" : $s).
+	  '</a>';
      }
 }
 
