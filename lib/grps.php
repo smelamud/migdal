ab6050faf75f0fc9f4c38ac7d4923ad4856c2bc2
Shmuel Melamud <balu@migdal.ru>
2001-08-06 22:26:14 +0000

    Forgotten files

---
+++ lib/grps.php
@@ -0,0 +1,82 @@
+<?php
+define('GRP_ANY',0);
+define('GRP_FORUMS',1);
+define('GRP_NEWS',2);
+define('GRP_GALLERY',4);
+define('GRP_EVENTS',7);
+define('GRP_POLL',8);
+define('GRP_COMPLAIN',8);
+
+$grpNames=array(GRP_FORUMS   => 'forums',
+                GRP_NEWS     => 'news',
+                GRP_GALLERY  => 'gallery',
+		GRP_POLL     => 'poll',
+		GRP_COMPLAIN => 'complain');
+
+function getGrpName($grp)
+{
+global $grpNames;
+
+return $grpNames[$grp];
+}
+
+function getGrpNames($grp)
+{
+global $grpNames;
+
+$names=array();
+foreach($grpNames as $key => $value)
+       if($grp & $key)
+         $names[]=$value;
+return $names;
+}
+
+function getGrpNumbers($grp)
+{
+global $grpNames;
+
+$ns=array();
+foreach(array_keys($grpNames) as $key)
+       if($grp & $key)
+         $ns[]=$key;
+return $ns;
+}
+
+function enclose($vals,$prefix='',$postfix='')
+{
+$result=array();
+foreach($vals as $val)
+       $result[]=$prefix.$val.$postfix;
+return $result;
+}
+
+function getPackedGrpFilter($grp,$prefix='')
+{
+return $grp==GRP_ANY ? ''
+                     : 'and ('.
+		       join(' or ',enclose(getGrpNumbers($grp),$prefix.'grp=')).
+		       ')';
+}
+
+function getUnpackedGrpFilter($grp,$prefix='')
+{
+return $grp==GRP_ANY ? ''
+                     : 'and ('.
+		       join(' or ',enclose(getGrpNames($grp),$prefix.'no_','=0')).
+		       ')';
+}
+
+$grpClassNames=array(GRP_FORUMS   => 'Forum',
+                     GRP_NEWS     => 'News',
+                     GRP_GALLERY  => 'Gallery',
+		     GRP_POLL     => 'Poll',
+		     GRP_COMPLAIN => 'Complain');
+
+function getGrpClassName($grp)
+{
+global $grpClassNames;
+
+$name=$grpClassNames[$grp];
+return isset($name) ? $name : 'Message';
+}
+?>
