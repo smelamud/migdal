466e5745fbc80a7cf1b0dd98b4019347593e237c
Shmuel Melamud <balu@migdal.ru>
2001-08-12 17:26:36 +0000

    No cache

---
+++ lib/no-cache.php
@@ -0,0 +1,8 @@
+<?php
+# @(#) $Id$
+
+header('Expires: '.gmdate('D, d M Y H:i:s',time()-60));
+header('Last-Modified: '.gmdate('D, d M Y H:i:s'));
+header('Cache-Control: no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0, s-max-age=0'); 
+header('Pragma: no-cache'); 
+?>
