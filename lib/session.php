9b7e9c764b5380facdd7fbe26da92863df86f2f2
Shmuel Melamud <balu@migdal.ru>
2001-08-18 20:04:44 +0000

    More correct and pretty user rights implementation

--- lib/session.php
+++ lib/session.php
@@ -5,62 +5,32 @@ require_once('conf/migdal.conf');
 
 require_once('lib/utils.php');
 
-$userSetDBNames=array('mp' => 'msg_portion',
-                      'fp' => 'forum_portion',
-		      'cp' => 'complain_portion');
-$userSetGlobalNames=array('mp' => 'MsgPortion',
-                          'fp' => 'ForumPortion',
-			  'cp' => 'ComplainPortion');
+$userRightNames=array('login','hidden','admin_users','admin_topics',
+                      'moderator','judge');
+$userSetNames=array('mp' => 'msg_portion',
+                    'fp' => 'forum_portion',
+                    'cp' => 'complain_portion');
 $userSetDefaults=array('mp' => 10,
                        'fp' => 10,
 		       'cp' => 20);
 
-function userSettings()
+function getProperName($name)
 {
-global $userId,$HTTP_GET_VARS,$HTTP_COOKIE_VARS,
-       $userSetDBNames,$userSetGlobalNames,$userSetDefaults;
-       
-if($userId>0)
-  {
-  $result=mysql_query('select '.join(',',$userSetDBNames).
-                     " from users
-		       where id=$userId")
-	       or die('Ошибка SQL при выборке установок пользователя');
-  $row=mysql_fetch_assoc($result);
-  }
-else
-  $row=array();
-$update=array();
-foreach($userSetGlobalNames as $key => $name)
-       {
-       $par=$HTTP_GET_VARS[$key];
-       $cookie=$HTTP_COOKIE_VARS["cookie$key"];
-       $db=$row[$userSetDBNames[$key]];
-       settype($par,'integer');
-       settype($cookie,'integer');
-       $glob=!empty($par) ? $par :
-            (!empty($cookie) ? $cookie :
-            (!empty($db) ? $db : $userSetDefaults[$key]));
-       if($userId>0 && $db!=$glob)
-         $update[$userSetDBNames[$key]]=$glob;
-       if($cookie!=$glob)
-         SetCookie("cookie$key",$glob,time()+3600*24*366,'/');
-       $GLOBALS["user$name"]=$glob;
-       }
-if($userId>0 && count($update)!=0)
-  mysql_query('update users
-               set '.makeKeyValue(',',$update).
-	     " where id=$userId")
-       or die('Ошибка SQL при сохранении установок пользователя');
+$parts=explode('_',$name);
+$proper='';
+foreach($parts as $part)
+       $proper.=ucfirst($part);
+return $proper;
 }
 
-function session()
+function userRights()
 {
-global $sessionid,
-       $userId,$userHidden,$userLogin,$userAdminUsers,$userAdminTopics,
-       $userModerator,$userJudge;
+global $sessionid,$userId,$userRightNames;
 
 settype($sessionid,'integer');
+
+foreach($userRightNames as $name)
+       $GLOBALS['user'.getProperName($name)]='';
 
 if(!$sessionid)
   $userId=-1;
@@ -76,20 +46,64 @@ else
   else
     {
     $userId=mysql_result($result,0,0);
-    $rights=mysql_query("select login,hidden,admin_users,admin_topics,
-                                moderator,judge
-			 from users
+    $rights=mysql_query('select '.join(',',$userRightNames).
+		       " from users
 			 where id=$userId")
 		 or die('Ошибка SQL при получении прав пользователя');
-    list($userLogin,$userHidden,$userAdminUsers,$userAdminTopics,
-         $userModerator,$userJudge)=mysql_fetch_row($rights);
-    if($userAdminUsers)
-      $userHidden--;
+    $info=mysql_fetch_assoc($rights);
+    foreach($info as $name => $value)
+           $GLOBALS['user'.getProperName($name)]=$value;
+    if($GLOBALS['userAdminUsers'])
+      $GLOBALS['userHidden']--;
     mysql_query("update sessions set last=null where sid=$sessionid")
 	 or die('Ошибка SQL при обновлении TIMESTAMP сессии');
     SetCookie('sessionid',$sessionid,time()+($sessionTimeout+1)*3600,'/');
     }
   }
+}
+
+function userSettings()
+{
+global $userId,$HTTP_GET_VARS,$HTTP_COOKIE_VARS,
+       $userSetNames,$userSetDefaults;
+       
+if($userId>0)
+  {
+  $result=mysql_query('select '.join(',',$userSetNames).
+                     " from users
+		       where id=$userId")
+	       or die('Ошибка SQL при выборке установок пользователя');
+  $row=mysql_fetch_assoc($result);
+  }
+else
+  $row=array();
+$update=array();
+foreach($userSetNames as $key => $name)
+       {
+       $par=$HTTP_GET_VARS[$key];
+       $cookie=$HTTP_COOKIE_VARS["cookie$key"];
+       $db=$row[$name];
+       settype($par,'integer');
+       settype($cookie,'integer');
+       $glob=!empty($par) ? $par :
+            (!empty($cookie) ? $cookie :
+            (!empty($db) ? $db : $userSetDefaults[$key]));
+       if($userId>0 && $db!=$glob)
+         $update[$name]=$glob;
+       if($cookie!=$glob)
+         SetCookie("cookie$key",$glob,time()+3600*24*366,'/');
+       $GLOBALS['user'.getProperName($name)]=$glob;
+       }
+if($userId>0 && count($update)!=0)
+  mysql_query('update users
+               set '.makeKeyValue(',',$update).
+	     " where id=$userId")
+       or die('Ошибка SQL при сохранении установок пользователя');
+}
+
+function session()
+{
+userRights();
 userSettings();
 }
 ?>
