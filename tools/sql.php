e323ab7200bec004d041a475a27d4ea5eb3acd95
Shmuel Melamud <balu@migdal.ru>
2001-09-03 23:55:35 +0000

    Database management utility implemented

---
+++ tools/sql.php
@@ -0,0 +1,115 @@
+<?php
+# @(#) $Id$
+
+error_reporting(0);
+
+function execute()
+{
+global $dbhost,$dbuser,$dbpasswd,$dbname,$sql,$result;
+
+$link=mysql_connect($dbhost,$dbuser,$dbpasswd);
+if(!$link)
+  return;
+if(!mysql_select_db($dbname))
+  return;
+$result=mysql_query($sql);
+}
+
+if($sql!='')
+  execute();
+
+if($dbname=='')
+  $dbname='migdal';
+if($dbuser=='')
+  $dbuser='root';
+if($dbhost=='')
+  $dbhost='localhost';
+?>
+<html>
+<head><title>Migdal - SQL</title></head>
+<body>
+ <table>
+  <form method=post action='<?php echo $SCRIPT_NAME ?>#error'>
+   <tr>
+    <td>Database</td>
+    <td>
+     <input type=text name='dbname' size=30 value='<?php echo $dbname ?>'>
+    </td>
+   </tr>
+   <tr>
+    <td>User</td>
+    <td>
+     <input type=text name='dbuser' size=30 value='<?php echo $dbuser ?>'>
+    </td>
+   </tr>
+   <tr>
+    <td>Password</td>
+    <td>
+     <input type=password name='dbpasswd' size=30
+            value='<?php echo $dbpasswd ?>'>
+    </td>
+   </tr>
+   <tr>
+    <td>Host</td>
+    <td>
+     <input type=text name='dbhost' size=30 value='<?php echo $dbhost ?>'>
+    </td>
+   </tr>
+   <tr><td colspan=2>Query</td></tr>
+   <tr><td colspan=2>
+    <textarea name='sql' rows=10 cols=50><?php echo $sql ?></textarea>
+   </td></tr>
+   <tr><td colspan=2>
+    <input type=submit value='Execute'><input type=reset value='Reset'>
+   </td></tr>
+  </form>
+ </table>
+ <a name='error'>
+ <?php
+ if(mysql_errno()!=0)
+   {
+   ?>
+   <font color=red><?php echo mysql_errno().': '.mysql_error() ?></font>
+   <?php
+   }
+ else
+   {
+   ?>
+   <p>
+   <table border=1>
+   <tr>
+   <?php
+   $n=mysql_num_fields($result);
+   for($i=0;$i<$n;$i++)
+      {
+      ?>
+      <th>
+      <?php echo mysql_field_name($result,$i) ?>
+      </th>
+      <?
+      }
+   ?>
+   </tr>
+   <?php
+   while($row=mysql_fetch_row($result))
+        {
+	?>
+	<tr>
+	<?php
+	foreach($row as $value)
+	       {
+	       ?>
+	       <td><?php echo $value ?></td>
+	       <?php
+	       }
+	?>
+	</tr>
+	<?php
+	}
+   ?>
+   </table>
+   <?php
+   }
+ ?>
+</body>
+</html>
