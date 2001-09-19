fedd6d6ee1041cf8105c8f5609ca10be34a8e27e
Shmuel Melamud <balu@migdal.ru>
2001-09-19 19:36:13 +0000

    Calendar functions reimplemented

---
+++ lib/unix-calendar.php
@@ -0,0 +1,20 @@
+<?php
+# @(#) $Id$
+
+define('UNIX_SDN_OFFSET',2440588);
+define('UNIX_DAY_SECONDS',24*60*60);
+
+function SDNToUNIX($sdn)
+{
+return ($sdn>=UNIX_SDN_OFFSET) ? (($sdn-UNIX_SDN_OFFSET)*UNIX_DAY_SECONDS)
+                              : false;
+}
+
+function UNIXToSDN($time=0)
+{
+if($time==0)
+  $time=time();
+return ((int)($time/UNIX_DAY_SECONDS))+UNIX_SDN_OFFSET;
+}
+
+?>
