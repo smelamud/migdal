5879a4f1f301fd8d2f690d1fe08210cfce3b39d9
Shmuel Melamud <balu@migdal.ru>
2001-10-05 21:48:47 +0000

    Additional decorations

--- lib/text.php
+++ lib/text.php
@@ -5,7 +5,8 @@ define('TF_PLAIN',0);
 define('TF_TEX',1);
 define('TF_HTML',2);
 define('TF_PRE',3);
-define('TF_MAX',3);
+define('TF_MAIL',4);
+define('TF_MAX',4);
 
 function is_delim($c)
 {
@@ -34,6 +35,19 @@ if($tag)
 return $c;
 }
 
+function replaceURLs($s)
+{
+$c=preg_replace('/\S+:\/\/\S+/','<a href="\\0">\\0</a>',$s);
+$c=preg_replace('/[A-Za-z-]+(\.[A-Za-z-]+)*@[A-Za-z-]+(\.[A-Za-z-]+)*/',
+                '<a href="mailto:\\0">\\0</a>',$c);
+return $c;
+}
+
+function replaceQuoting($s)
+{
+return preg_replace('/(&gt;.*)\n/','<i>\\1</i>'."\n",$s);
+}
+
 function textToStotext($format,$s)
 {
 return $format==TF_HTML ? $s : htmlspecialchars($s,ENT_QUOTES);
@@ -45,10 +59,12 @@ $c=$s;
 switch($format)
       {
       default:
+      case TF_MAIL:
+	   $c=replaceQuoting($c);
       case TF_PLAIN:
+	   $c=replaceURLs($c);
 	   $c=str_replace("\n\n",'<p>',$c);
 	   $c=str_replace("\n",'<br>',$c);
-      case TF_PRE:
 	   $c=flipReplace('_','<u>','</u>',$c);
 	   $c=flipReplace('~','<b>','</b>',$c);
 	   break;
@@ -57,9 +73,15 @@ switch($format)
 	   $c=str_replace('\\\\','<br>',$c);
 	   $c=flipReplace('_','<u>','</u>',$c);
 	   $c=flipReplace('~','<b>','</b>',$c);
+	   $c=replaceURLs($c);
 	   break;
       case TF_HTML:
 	   $c=str_replace("\n\n",'<p>',$c);
+	   break;
+      case TF_PRE:
+	   $c=replaceURLs($c);
+	   $c=flipReplace('_','<u>','</u>',$c);
+	   $c=flipReplace('~','<b>','</b>',$c);
 	   break;
       }
 return $c;
