281968e5ff081176268c7baf96123e0ba804cab9
Shmuel Melamud <balu@migdal.ru>
2001-11-09 11:26:24 +0000

    Ликвидированы последние сообщения в lib/grputils.php

--- lib/grputils.php
+++ lib/grputils.php
@@ -1,31 +1,6 @@
 <?php
 require_once('lib/grps.php');
 
-$grpItemTitles=array(GRP_FORUMS   => 'сообщение',
-                     GRP_NEWS     => 'новость',
-		     GRP_GALLERY  => 'картинку',
-		     GRP_ARTICLES => 'статью',
-		     GRP_ALL      => 'сообщение');
-$grpIdents=array(GRP_FORUMS   => 'forums',
-                 GRP_NEWS     => 'news',
-		 GRP_GALLERY  => 'gallery',
-		 GRP_ARTICLES => 'articles',
-                 GRP_ALL      => 'messages');
-
-function getGrpItemTitle($grp)
-{
-global $grpItemTitles;
-
-return $grpItemTitles[$grp];
-}
-
-function getGrpIdent($grp)
-{
-global $grpIdents;
-
-return $grpIdents[$grp];
-}
-
 class GrpData
 {
 var $grp;
@@ -60,16 +35,6 @@ return $this->ignore ? 1 : 0;
 function getInvIgnore()
 {
 return $this->ignore ? 0 : 1;
-}
-
-function getItemTitle()
-{
-return getGrpItemTitle($this->grp);
-}
-
-function getIdent()
-{
-return getGrpIdent($this->grp);
 }
 
 function getClassName()
