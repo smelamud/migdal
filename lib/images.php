5c5cf837ceef03ce3a6471b626c31c76a882083d
Shmuel Melamud <balu@migdal.ru>
2002-05-07 12:45:20 +0000

    Beauty fix

--- lib/images.php
+++ lib/images.php
@@ -209,7 +209,8 @@ $result=mysql_query("select format
                      from images
  	             where id=$id");
 $ext=getImageExtension(mysql_result($result,0,0));
-return "<a href='lib/image.php/$uiName-$id.$ext?id=$id&size=large' target=_blank>";
+return "<a href='lib/image.php/$uiName-$id.$ext?id=$id&size=large' target=_blank".
+       " title='Увеличить'>";
 }
 
 function imageSetExists($image_set)
