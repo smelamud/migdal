331ee0d349fe495f6776e30c70b170e694e82079
Shmuel Melamud <balu@migdal.ru>
2002-06-11 22:41:01 +0000

    Админу высылается уведомление о подтверждении регистрации юзером

--- actions/userconfirm.php
+++ actions/userconfirm.php
@@ -6,17 +6,18 @@ require_once('lib/database.php');
 require_once('lib/session.php');
 require_once('lib/post.php');
 require_once('lib/errors.php');
+require_once('lib/mailings.php');
 
 function confirmUser($id,$code,&$userLogin)
 {
 global $userAdminUsers;
 
 if($userAdminUsers && $id!=0)
-  $result=mysql_query("select login
+  $result=mysql_query("select login,id
 		       from users
 		       where id=$id and hidden<2");
 else
-  $result=mysql_query('select login
+  $result=mysql_query('select login,id
 		       from users
 		       where confirm_code=\''.addslashes($code).'\'
 			     and hidden<2');
@@ -25,11 +26,13 @@ if(!$result)
 if(mysql_num_rows($result)<=0)
   return EUC_NO_USER;
 $userLogin=mysql_result($result,0,0);
+$id=mysql_result($result,0,1);
 $result=mysql_query("update users
                      set no_login=0,hidden=0,confirm_deadline=null
 		     where login='$userLogin'");
 if(!$result)
   return EUC_SQL_CONFIRM;
+sendMailAdmin(MAIL_CONFIRMED,'admin_users',$id);
 return EUC_OK;
 }
 
