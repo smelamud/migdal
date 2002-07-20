45a6a8e8c9ef573f090986bf13b276f246197886
Shmuel Melamud <balu@migdal.ru>
2002-07-20 21:50:41 +0000

    Extended logging of traps

--- upgrade-0.1-0.2.sql
+++ upgrade-0.1-0.2.sql
@@ -57,3 +57,4 @@ ALTER TABLE `users` ADD `admin_domain` TINYINT NOT NULL AFTER `judge`;
 update mailing_types set text='register.php' where text='register.mail';
 update mailing_types set text='registering.php' where text='registering.mail';
 drop table mailing_types;
+ALTER TABLE `logs` ADD `referer` VARCHAR(250) NOT NULL;
