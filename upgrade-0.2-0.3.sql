23c66686808a805e8ec9997d61771b4212b8aec6
Shmuel Melamud <balu@migdal.ru>
2002-08-18 22:39:02 +0000

    Еще таблицы

--- upgrade-0.2-0.3.sql
+++ upgrade-0.2-0.3.sql
@@ -21,3 +21,18 @@ ALTER TABLE `messages` ADD `lang` CHAR(5) NOT NULL AFTER `track`;
 ALTER TABLE `topics` ADD `virtual` TINYINT NOT NULL AFTER `hidden`;
 ALTER TABLE `topics` ADD `index0` INT NOT NULL, ADD `index1` INT NOT NULL, ADD `index2` INT NOT NULL, ADD `index3` INT NOT NULL, ADD `index4` INT NOT NULL;
 ALTER TABLE `topics` ADD INDEX (`index0`, `index1`, `index2`, `index3`, `index4`);
+CREATE TABLE `multisites` (
+ `domain` VARCHAR(70) NOT NULL, 
+ `replacer` VARCHAR(70) NOT NULL,
+ INDEX (`domain`)
+ );
+CREATE TABLE `links` (
+ `id` INT NOT NULL, 
+ `message_id` INT NOT NULL, 
+ `title` VARCHAR(250) NOT NULL, 
+ `url` VARCHAR(250) NOT NULL, 
+ `url_check` DATETIME NOT NULL, 
+ `url_check_success` DATETIME NOT NULL,
+ PRIMARY KEY (`id`),
+ INDEX (`message_id`, `url_check`, `url_check_success`)
+ );
