0e809941c5aa97cf189d02b8e2c773ede7b96817
Shmuel Melamud <balu@migdal.ru>
2002-11-01 12:42:26 +0000

    Не транслировать HTML при выдаче автору

--- lib/plain.php
+++ lib/plain.php
@@ -5,15 +5,19 @@ require_once('conf/migdal.conf');
 
 require_once('lib/database.php');
 require_once('lib/bug.php');
+require_once('lib/text.php');
 
 dbOpen();
 $id=addslashes($id);
-$result=mysql_query("select large_body
+$result=mysql_query("select large_body,large_format
                      from stotexts
 		     where id=$id")
 	  or sqlbug('Ошибка SQL при выборке текста');
 header('Content-Type: text/plain');
-echo strtr(mysql_result($result,0,0),
-           array_flip(get_html_translation_table(HTML_ENTITIES,ENT_QUOTES)));
+if(mysql_result($result,0,1)==TF_HTML)
+  echo mysql_result($result,0,0);
+else
+  echo strtr(mysql_result($result,0,0),
+	     array_flip(get_html_translation_table(HTML_ENTITIES,ENT_QUOTES)));
 dbClose();
 ?>
