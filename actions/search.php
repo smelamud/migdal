1f636ce0042cf4595f15ee9d30506d6880c4107f
Shmuel Melamud <balu@migdal.ru>
2003-07-07 20:52:06 +0000

    Поиск журналируется

--- actions/search.php
+++ actions/search.php
@@ -7,9 +7,11 @@ require_once('lib/session.php');
 require_once('lib/post.php');
 
 postString('q');
+postInteger('offset');
 
 dbOpen();
 session();
+logEvent('search',"offset=$offset query=".urlencode($q));
 header('Location: '.remakeMakeURI($okdir,
                                   $HTTP_POST_VARS,
 				  array('okdir',
