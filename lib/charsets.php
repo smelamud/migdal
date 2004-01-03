fa84b0cc203b69b9ae308f222b749d0421c67cdb
Shmuel Melamud <balu@migdal.ru>
2004-01-03 21:10:29 +0000

    Rollback

--- lib/charsets.php
+++ lib/charsets.php
@@ -70,11 +70,10 @@ $hebrewCodes=array(
                    'С' => '05EA',
                    '+' => 'FB29',
                    '-' => '05BE',
-		   "&#039;" => '05F3',
+		   "'" => '05F3',
 		   '"' => '05F4'
                   );
 
-// Использовать только для обработки вывода convertOutput()!
 function convertHebrew($s,$htmlEntities=true)
 {
 global $hebrewCodes;
