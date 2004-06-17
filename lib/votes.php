a9dc9059075a1d93d07e85858adb588db154c5c3
Shmuel Melamud <balu@migdal.ru>
2004-06-17 08:39:43 +0000

    Минимальная оценка - 1, а не 0

--- lib/votes.php
+++ lib/votes.php
@@ -5,14 +5,14 @@ require_once('lib/ip.php');
 
 function getRatingSQL($vote,$vote_count)
 {
-return "if($vote_count=0,2.5,".
-       "$vote_count/($vote_count+1)*($vote/$vote_count-2.5)+2.5)";
+return "if($vote_count=0,3,".
+       "$vote_count/($vote_count+1)*($vote/$vote_count-3)+3)";
 }
 
 function getRating($vote,$vote_count)
 {
-return $vote_count==0 ? 2.5
-                      : $vote_count/($vote_count+1)*($vote/$vote_count-2.5)+2.5;
+return $vote_count==0 ? 3
+                      : $vote_count/($vote_count+1)*($vote/$vote_count-3)+3;
 }
 
 function getVote($id)
