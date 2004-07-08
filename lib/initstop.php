574b25cf2bbc36d493dcfe696bbf9738ae1d981f
Shmuel Melamud <balu@migdal.ru>
2004-07-08 08:19:06 +0000

    Инициализация/отправка страницы выделены в отдельный файл. dbClose() поставлен перед отправкой страницы.

---
+++ lib/initstop.php
@@ -0,0 +1,59 @@
+<?php
+# @(#) $Id$
+
+require_once('lib/errorreporting.php');
+require_once('lib/database.php');
+require_once('lib/session.php');
+require_once('lib/no-cache.php');
+require_once('lib/charsets.php');
+require_once('lib/style.php');
+require_once('lib/redirs.php');
+
+function initialize()
+{
+noCacheHeaders();
+dbOpen();
+session();
+set_error_handler('error_handler');
+ob_start();
+}
+
+function initializeMeta()
+{
+echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">\n";
+noCacheMeta();
+}
+
+function initializeHead()
+{
+global $stylesheetList,$userStyle;
+
+redirect();
+foreach($stylesheetList as $sheet)
+       {
+       echo "<link rel='stylesheet' href='/styles/$sheet-".
+             getStyle($userStyle).".css'>";
+       include("styles/$sheet-".getStyle($userStyle).'.php');
+       }
+}
+
+function finalize()
+{
+global $userReadKOI;
+
+dbClose();
+$Output=ob_get_contents();
+ob_end_clean();
+echo convertOutput($Output,$userReadKOI>0);
+}
+
+function error_handler($errno,$errstr,$errfile,$errline)
+{
+if(($errno & error_reporting())==0)
+  return;
+logEvent('bug',"$errstr file($errfile) line($errline)");
+echo "<b>Fatal error</b>: $errstr in <b>$errfile</b> on line <b>$errline</b>";
+finalize();
+die();
+}
+?>
