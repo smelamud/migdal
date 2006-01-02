0b3781e7ece82c4081794b27eb81c80dd74fefd0
Shmuel Melamud <balu@migdal.ru>
2006-01-02 15:06:15 +0000

    Диспетчер

--- migdal/dispatcher.php
+++ migdal/dispatcher.php
@@ -3,10 +3,36 @@
 
 require_once('lib/structure.php');
 
-$info=getLocationInfo($_SERVER['REQUEST_URI']);
-$ScriptName=$info->getScript();
+$href=$_SERVER['REQUEST_URI'];
+$parts=parse_url($href);
+$RequestPath=$parts['path'];
+$ScriptName='';
+if($RequestPath=='/dispatcher.php')
+  $ScriptName='404.php';
+elseif(substr($RequestPath,-4)=='.php')
+  {
+  $ScriptName=substr($RequestPath,1);
+  if(!file_exists($ScriptName))
+    $ScriptName='404.php';
+  }
+elseif(substr($RequestPath,-1)!='/' && substr($RequestPath,5)!='.html')
+  {
+  $href="$RequestPath/";
+  if(isset($parts['query']) && $parts['query']!='')
+    $href.='?'.$parts['query'];
+  if(isset($parts['fragment']) && $parts['fragment']!='')
+    $href.='#'.$parts['fragment'];
+  header("Location: $href");
+  }
+else
+  {
+  //TODO it's possible to get backtrace here
+  $info=getLocationInfo($href);
+  $ScriptName=$info->getScript();
+  unset($info);
+  }
+unset($parts);
+unset($href);
 if($ScriptName!='')
   include($ScriptName);
-else
-  include('404.php');
 ?>
