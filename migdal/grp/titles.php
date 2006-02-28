53027541dbf71260ff2e8abd48df17482fa398ea
Shmuel Melamud <balu@migdal.ru>
2006-02-28 16:34:40 +0000

    Каталог ссылок - следующий шаг

--- migdal/grp/titles.php
+++ migdal/grp/titles.php
@@ -8,4 +8,10 @@ function titleTopicName($id)
 $topic=getTopicNameById($id);
 return $topic->getSubject();
 }
+
+function titleTopicBodyOrName($id)
+{
+$topic=getTopicById($id);
+return $topic->getBody()=='' ? $topic->getSubject() : $topic->getBody();
+}
 ?>
