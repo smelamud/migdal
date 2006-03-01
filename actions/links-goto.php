92f0236be55cd442883d7e94e62abac9d8607503
Shmuel Melamud <balu@migdal.ru>
2006-03-01 15:46:10 +0000

    fastjump реализован

--- actions/links-goto.php
+++ actions/links-goto.php
@@ -1,18 +1,23 @@
 <?php
 # @(#) $Id$
 
-# This is a template of action page
-
 require_once('lib/errorreporting.php');
 require_once('lib/database.php');
 require_once('lib/session.php');
 require_once('lib/post.php');
+require_once('lib/catalog.php');
 
 postString('okdir');
 postString('faildir');
 
+postInteger('topic_id');
+
 dbOpen();
 session();
-header("Location: $okdir");
+$catalog=catalogById($topic_id);
+if($catalog!='')
+  header("Location: $okdir$catalog");
+else
+  header("Location: $faildir");
 dbClose();
 ?>
