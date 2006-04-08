edb504f23e20af4a888a83655bcd8e944883933a
Shmuel Melamud <balu@migdal.ru>
2006-04-08 22:22:04 +0000

    Fix

--- lib/errorreporting.php
+++ lib/errorreporting.php
@@ -3,6 +3,5 @@
 
 require_once('conf/migdal.conf');
 
-//error_reporting(!$debug ? E_ERROR|E_PARSE|E_USER_ERROR : E_ALL);
-error_reporting(E_ALL);
+error_reporting(!$debug ? E_ERROR|E_PARSE|E_USER_ERROR : E_ALL);
 ?>
