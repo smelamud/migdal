814f092299851449c5d891f33a096accbd2420b1
Shmuel Melamud <balu@migdal.ru>
2007-03-16 01:21:36 +0000

    Mail template improvements

--- lib/initstop-mail.php
+++ lib/initstop-mail.php
@@ -4,26 +4,16 @@
 require_once('lib/errorreporting.php');
 require_once('lib/database.php');
 require_once('lib/session.php');
-require_once('lib/mail.php');
-require_once('lib/post.php');
 
 function initializeMail()
 {
-global $args;
-
-commandLineArgs();
-
-parse_str($args[0]);
-dbOpen();
-session($args[1]);
 ob_start();
 }
 
 function finalizeMail()
 {
-dbClose();
 $Output=ob_get_contents();
 ob_end_clean();
-echo convertMailOutput($Output);
+return convertOutput($Output);
 }
 ?>
