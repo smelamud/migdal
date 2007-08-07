03623b704b84791c6bdc485babbecf9d0416a857
Shmuel Melamud <balu@migdal.ru>
2007-08-07 16:10:41 +0000

    Константы текстовых форматов выделены в отдельный файл

---
+++ lib/text-formats.php
@@ -0,0 +1,13 @@
+<?php
+# @(#) $Id$
+
+define('TF_PLAIN',0);
+define('TF_TEX',1);
+define('TF_XML',2);
+define('TF_MAIL',3);
+define('TF_MAX',3);
+
+define('MTEXT_LINE',1);
+define('MTEXT_SHORT',2);
+define('MTEXT_LONG',3);
+?>
--- lib/text.php
+++ lib/text.php
@@ -2,16 +2,7 @@
 # @(#) $Id$
 
 require_once('lib/array.php');
-
-define('TF_PLAIN',0);
-define('TF_TEX',1);
-define('TF_XML',2);
-define('TF_MAIL',3);
-define('TF_MAX',3);
-
-define('MTEXT_LINE',1);
-define('MTEXT_SHORT',2);
-define('MTEXT_LONG',3);
+require_once('lib/text-formats.php');
 
 function ellipsize($s,$len)
 {
