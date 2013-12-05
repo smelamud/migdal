5ca8f6b6e1b1b2884fe87d79e52de9c52a396a37
Shmuel Melamud <balu@migdal.ru>
2013-12-05 22:15:31 +0000

    Do not generate PHP files from MCSS stylesheets

--- lib/initstop.php
+++ lib/initstop.php
@@ -11,57 +11,48 @@ require_once('lib/redirs.php');
 require_once('lib/post.php');
 require_once('lib/logs.php');
 
-function initialize()
-{
-noCacheHeaders();
-header('Content-Style-Type: text/css'); 
-dbOpen();
-session();
-httpRequestInteger('err');
-set_error_handler('error_handler');
-ob_start('convertOutput');
+function initialize() {
+    noCacheHeaders();
+    header('Content-Style-Type: text/css'); 
+    dbOpen();
+    session();
+    httpRequestInteger('err');
+    set_error_handler('error_handler');
+    ob_start('convertOutput');
 }
 
-function initializeMeta()
-{
-echo "<!DOCTYPE html>\n";
+function initializeMeta() {
+    echo "<!DOCTYPE html>\n";
 }
 
-function initializeHead()
-{
-global $stylesheetList,$userStyle,$jsList,$ogImageList;
+function initializeHead() {
+    global $stylesheetList, $userStyle, $jsList, $ogImageList;
 
-foreach($stylesheetList as $sheet)
-       {
-       echo "<link rel='stylesheet' href='/styles/$sheet-".
-             getStyle($userStyle).".css'>\n";
-       include("styles/$sheet-".getStyle($userStyle).'.php');
-       }
-if(isset($jsList))
-  foreach($jsList as $src)
-         echo "<script src='$src' type='text/javascript'></script>\n";
-if(isset($ogImageList) && count($ogImageList)>0)
-  {
-  echo "<link rel='image_src' href='{$ogImageList[0]}'>\n";
-  foreach($ogImageList as $src)
-         echo "<meta property='og:image' content='$src'>\n";
-  }
+    foreach ($stylesheetList as $sheet)
+        echo "<link rel='stylesheet' href='/styles/$sheet-".
+              getStyle($userStyle).".css'>\n";
+    if (isset($jsList))
+        foreach ($jsList as $src)
+            echo "<script src='$src' type='text/javascript'></script>\n";
+    if (isset($ogImageList) && count($ogImageList) > 0) {
+        echo "<link rel='image_src' href='{$ogImageList[0]}'>\n";
+        foreach ($ogImageList as $src)
+            echo "<meta property='og:image' content='$src'>\n";
+    }
 }
 
-function finalize()
-{
-dbClose(); // dbClose() должен находиться здесь, чтобы профайлер не учитывал
-           // время скачивания страницы клиентом
-ob_end_flush();
+function finalize() {
+    dbClose(); // dbClose() должен находиться здесь, чтобы профайлер не учитывал
+               // время скачивания страницы клиентом
+    ob_end_flush();
 }
 
-function error_handler($errno,$errstr,$errfile,$errline)
-{
-if(($errno & error_reporting())==0)
-  return;
-logEvent('bug',"$errstr file($errfile) line($errline)");
-echo "<b>Fatal error</b>: $errstr in <b>$errfile</b> on line <b>$errline</b>";
-finalize();
-die();
+function error_handler($errno, $errstr, $errfile, $errline) {
+    if (($errno & error_reporting()) == 0)
+        return;
+    logEvent('bug', "$errstr file($errfile) line($errline)");
+    echo "<b>Fatal error</b>: $errstr in <b>$errfile</b> on line <b>$errline</b>";
+    finalize();
+    die();
 }
 ?>
