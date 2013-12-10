357c55388cbb15de46c79849290e95f8e0d2d087
Shmuel Melamud <balu@migdal.ru>
2013-12-10 12:16:44 +0000

    Fixed fuzzy calendar

--- lib/fuzzy-calendar.php
+++ lib/fuzzy-calendar.php
@@ -14,25 +14,30 @@ function formatFuzzyTimeElapsed($time, $format/*for future extensions*/ = '') {
     if ($diff == 1)
         return 'минуту назад';
     if ($diff < 60)
-        return $diff.getPlural($diff, array(' минуту', ' минуты', ' минут')).' назад';
+        return $diff.getPlural($diff, array(' минуту', ' минуты', ' минут')).
+               ' назад';
     $diff = (int)($diff / 60);
     if ($diff == 1)
         return 'час назад';
     if ($diff < 24)
-        return $diff.getPlural($diff, array(' час', ' часа', ' часов')).' назад';
+        return $diff.getPlural($diff, array(' час', ' часа', ' часов')).
+               ' назад';
     $diff = (int)($diff / 24);
     if ($diff == 1)
         return 'вчера';
     if ($diff == 2)
         return 'позавчера';
     if ($diff < 30)
-        return $diff.getPlural($diff, array('день', 'дня', 'дней')).' назад';
+        return $diff.getPlural($diff, array(' день', ' дня', ' дней')).
+               ' назад';
     if ($diff < 60)
         return 'два месяца назад';
     if ($diff < 90)
         return 'три месяца назад';
-    if (gmdate('Y', $time) == gmdate('Y', ourtime()))
-        return formatRussianDate('d k', $time);
-    return formatRussianDate('d k Y', $time).' г.';
+    $day = gmdate('d ', $time);
+    $year = gmdate('Y', $time);
+    if ($year == gmdate('Y', ourtime()))
+        return $day.formatRussianDate('k', $time);
+    return $day.formatRussianDate('k', $time)." $year г.";
 }
 ?>
