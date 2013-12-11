e1b9fab626e7d01b2616df97021561e9216b157c
Shmuel Melamud <balu@migdal.ru>
2013-12-11 12:11:54 +0000

    Formatting.

--- lib/tmptexts.php
+++ lib/tmptexts.php
@@ -4,37 +4,30 @@
 require_once('lib/bug.php');
 require_once('lib/sql.php');
 
-function tmpTextSave($text)
-{
-$textS=addslashes($text);
-sql("insert into tmp_texts(value)
-     values('$textS')",
-    __FUNCTION__);
-return sql_insert_id();
+function tmpTextSave($text) {
+    $textS = addslashes($text);
+    sql("insert into tmp_texts(value)
+         values('$textS')", __FUNCTION__);
+    return sql_insert_id();
 }
 
-function tmpTextRestore($id)
-{
-$result=sql("select value
-             from tmp_texts
-	     where id=$id",
-            __FUNCTION__,'restore');
-sql("update tmp_texts
-     set last_access=null
-     where id=$id",
-    __FUNCTION__,'timestamp');
-return mysql_num_rows($result)>0 ? mysql_result($result,0,0) : '';
+function tmpTextRestore($id) {
+    $result = sql("select value
+                   from tmp_texts
+                   where id=$id", __FUNCTION__, 'restore');
+    sql("update tmp_texts
+         set last_access=null
+         where id=$id", __FUNCTION__, 'timestamp');
+    return mysql_num_rows($result) > 0 ? mysql_result($result, 0, 0) : '';
 }
 
-function deleteObsoleteTmpTexts()
-{
-global $tmpTextTimeout;
-
-$now=sqlNow();
-sql("delete from tmp_texts
-     where last_access+interval $tmpTextTimeout hour<'$now'",
-    __FUNCTION__);
-sql('optimize table tmp_texts',
-    __FUNCTION__,'optimize');
+function deleteObsoleteTmpTexts() {
+    global $tmpTextTimeout;
+    
+    $now = sqlNow();
+    sql("delete from tmp_texts
+         where last_access+interval $tmpTextTimeout hour<'$now'", __FUNCTION__);
+    sql('optimize table tmp_texts', __FUNCTION__, 'optimize');
 }
 ?>
+
