437f3446ce343be2f2f7e19f98bf479ad765403e
Shmuel Melamud <balu@migdal.ru>
2014-06-12 12:58:27 +0000

    Formatting

--- lib/mtext-html.php
+++ lib/mtext-html.php
@@ -9,48 +9,7 @@ require_once('lib/callbacks.php');
 require_once('lib/mtext-callback-data.php');
 require_once('lib/text.php');
 require_once('lib/inner-images.php');
-
-$mtextRootTag = array(
-    MTEXT_LINE  => 'mtext-line',
-    MTEXT_SHORT => 'mtext-short',
-    MTEXT_LONG  => 'mtext-long'
-);
-$mtextTagLevel = array(
-    'MTEXT-LINE' => MTEXT_LINE,
-    'A'        => MTEXT_LINE,
-    'EMAIL'    => MTEXT_LINE,
-    'USER'     => MTEXT_LINE,
-    'IMG'      => MTEXT_LINE,
-    'B'        => MTEXT_LINE,
-    'I'        => MTEXT_LINE,
-    'U'        => MTEXT_LINE,
-    'S'        => MTEXT_LINE,
-    'STRIKE'   => MTEXT_LINE,
-    'TT'       => MTEXT_LINE,
-    'SUP'      => MTEXT_LINE,
-    'MTEXT-SHORT' => MTEXT_SHORT,
-    'P'        => MTEXT_SHORT,
-    'QUOTE'    => MTEXT_SHORT,
-    'CENTER'   => MTEXT_SHORT,
-    'H2'       => MTEXT_SHORT,
-    'H3'       => MTEXT_SHORT,
-    'H4'       => MTEXT_SHORT,
-    'BR'       => MTEXT_SHORT,
-    'UL'       => MTEXT_SHORT,
-    'OL'       => MTEXT_SHORT,
-    'DL'       => MTEXT_SHORT,
-    'LI'       => MTEXT_SHORT,
-    'OBJECT'   => MTEXT_SHORT,
-    'PARAM'    => MTEXT_SHORT,
-    'EMBED'    => MTEXT_SHORT,
-    'MTEXT-LONG' => MTEXT_LONG,
-    'FOOTNOTE' => MTEXT_LONG,
-    'TABLE'    => MTEXT_LONG,
-    'TR'       => MTEXT_LONG,
-    'TD'       => MTEXT_LONG,
-    'TH'       => MTEXT_LONG,
-    'INCUT'    => MTEXT_LONG
-);
+require_once('lib/mtext-tags.php');
 
 class InnerImageBlock {
 
--- lib/text-xml.php
+++ lib/text-xml.php
@@ -1,275 +1,218 @@
 <?php
 # @(#) $Id$
 
-$mtextEmptyTags=array('A'        => false,
-		      'EMAIL'    => true,
-		      'USER'     => true,
-		      'B'        => false,
-		      'I'        => false,
-		      'U'        => false,
-		      'S'        => false,
-		      'STRIKE'   => false,
-		      'TT'       => false,
-		      'SUP'      => false,
-		      'P'        => false,
-		      'QUOTE'    => false,
-		      'CENTER'   => false,
-		      'H2'       => false,
-		      'H3'       => false,
-		      'H4'       => false,
-		      'BR'       => true,
-		      'UL'       => false,
-		      'OL'       => false,
-		      'DL'       => false,
-		      'LI'       => false,
-		      'FOOTNOTE' => false,
-		      'IMG'      => true,
-		      'TABLE'    => false,
-		      'TR'       => false,
-		      'TD'       => false,
-		      'TH'       => false,
-		      'INCUT'    => false,
-		      'OBJECT'   => false,
-		      'PARAM'    => true,
-		      'EMBED'    => false);
+require_once('lib/mtext-tags.php');
 
-function replaceParagraphs($s)
-{
-$s=preg_replace('/\n\s*\n/','</p><p>',$s);
-$s=preg_replace('/<p>&lt;-/','<p clear="left">',$s);
-if(substr($s,0,5)=='&lt;-')
-  return '<p clear="left">'.substr($s,5).'</p>';
-else
-  return "<p>$s</p>";
+function replaceParagraphs($s) {
+    $s = preg_replace('/\n\s*\n/', '</p><p>', $s);
+    $s = preg_replace('/<p>&lt;-/', '<p clear="left">', $s);
+    if (substr($s,0,5) == '&lt;-')
+        return '<p clear="left">'.substr($s,5).'</p>';
+    else
+        return "<p>$s</p>";
 }
 
-class MTText
-{
-var $text;
+class MTText {
 
-function __construct($text)
-{
-$this->text=$text;
-}
+    private $text;
 
-function toString()
-{
-return $this->text;
-}
+    public function __construct($text) {
+        $this->text = $text;
+    }
+
+    public function appendText($text) {
+        $this->text .= $text;
+    }
+
+    public function toString() {
+        return $this->text;
+    }
 
 }
 
 class MTTag
-      extends MTText
-{
-var $name;
+        extends MTText {
 
-function __construct($name,$text)
-{
-parent::__construct($text);
-$this->name=$name;
-}
+    private $name;
 
-function getName()
-{
-return $this->name;
-}
-
-function getProperName()
-{
-$name=$this->name;
-if($name{0}=='/')
-  $name=substr($name,1);
-return $name;
-}
-
-function isClosing()
-{
-return $this->name{0}=='/';
-}
-
-}
-
-function isTagValid($name)
-{
-global $mtextEmptyTags;
-
-if($name=='')
-  return false;
-$name=strtoupper($name);
-if($name{0}=='/')
-  $name=substr($name,1);
-return isset($mtextEmptyTags[$name]);
-}
-
-function joinQueue($queue)
-{
-while(count($queue)>0)
-     {
-     if(get_class($queue[0])=='MTTag' && $queue[0]->getProperName()=='BR')
-       array_shift($queue);
-     elseif(count($queue)>=2
-	    && get_class($queue[0])=='MTTag' && $queue[0]->getName()=='P'
-	    && get_class($queue[1])=='MTTag' && $queue[1]->getName()=='/P')
-       {
-       array_shift($queue);
-       array_shift($queue);
-       }
-     else
-       break;
-     }
-while(count($queue)>0)
-     {
-     if(get_class($queue[count($queue)-1])=='MTTag'
-        && $queue[count($queue)-1]->getProperName()=='BR')
-       array_pop($queue);
-     elseif(count($queue)>=2
-	    && get_class($queue[count($queue)-1])=='MTTag'
-	    && $queue[count($queue)-1]->getName()=='/P'
-	    && get_class($queue[count($queue)-2])=='MTTag'
-	    && $queue[count($queue)-2]->getName()=='P')
-       {
-       array_pop($queue);
-       array_pop($queue);
-       }
-     else
-       break;
-     }
-$out='';
-foreach($queue as $obj)
-       $out.=$obj->toString();
-return $out;
-}
-
-function closeTag($tagName,&$tagStack,&$xmlQueue,$tagObject)
-{
-$rstack=array();
-while(count($tagStack)>0 && $tagStack[count($tagStack)-1]!=$tagName)
-     {
-     $tag=array_pop($tagStack);
-     $xmlQueue[]=new MTTag($tag,'</'.strtolower($tag).'>');
-     $rstack[]=$tag;
-     }
-if($tagName=='')
-  return;
-if(count($tagStack)==0)
-  $xmlQueue[]=new MTTag($tagName,'<'.strtolower($tagName).'>');
-else
-  array_pop($tagStack);
-$xmlQueue[]=$tagObject;
-while(count($rstack)>0)
-     {
-     $tag=array_pop($rstack);
-     $xmlQueue[]=new MTTag($tag,'<'.strtolower($tag).'>');
-     $tagStack[]=$tag;
-     }
-}
-
-function processTag($tag,&$tagStack,&$xmlQueue)
-{
-global $mtextEmptyTags;
-
-preg_match('/^<\s*(\/?\w+)\s*(.*)>$/',$tag,$m);
-if(!isset($m[1]) || !isTagValid($m[1]))
-  return false;
-$out='<'.$m[1];
-$tagName=strtoupper($m[1]);
-$tail=isset($m[2]) ? $m[2] : '';
-debugLog(LL_DEBUG,'processing tag: tagName=(%) tail=(%)',array($tagName,$tail));
-while($tail!='')
-     {
-     if(preg_match('/^([-\w]+)\s*=\s*("[^"]*"|\'[^\']*\')\s*(.*)$/',$tail,
-                   $m))
-       {
-       $out.=' '.$m[1].'='.$m[2];
-       $tail=isset($m[3]) ? $m[3] : '';
-       }
-     elseif(preg_match('/^([-\w]+)\s*=\s*(\S+)\s*(.*)$/',$tail,$m))
-       {
-       $out.=' '.$m[1].'="'.$m[2].'"';
-       $tail=isset($m[3]) ? $m[3] : '';
-       }
-     elseif(preg_match('/^([-\w]+)\s*(.*)$/',$tail,$m))
-       {
-       $out.=' '.$m[1].'="'.$m[1].'"';
-       $tail=isset($m[2]) ? $m[2] : '';
-       }
-     else
-       {
-       $tail=substr($tail,1);
-       }
-     }
-$tagObject=new MTTag($tagName,$out);
-if($tagName[0]=='/')
-  {
-  if(!$mtextEmptyTags[substr($tagName,1)])
-    {
-    $tagObject->text.='>';
-    closeTag(substr($tagName,1),$tagStack,$xmlQueue,$tagObject);
+    public function __construct($name, $text) {
+        parent::__construct($text);
+        $this->name = $name;
     }
-  // For empty tags closing tag is silently ignored
-  }
-else
-  if(!$mtextEmptyTags[$tagName])
-    {
-    $tagObject->text.='>';
-    $tagStack[]=$tagName;
-    $xmlQueue[]=$tagObject;
+
+    public function getName() {
+        return $this->name;
     }
-  else
-    {
-    $tagObject->text.=' />';
-    $xmlQueue[]=$tagObject;
+
+    public function getProperName() {
+        $name = $this->name;
+        if ($name{0} == '/')
+            $name = substr($name, 1);
+        return $name;
     }
-return true;
+
+    public function isClosing() {
+        return $this->name{0} == '/';
+    }
+
 }
 
-function cleanupXML($s)
-{
-global $mtextEmptyTags;
+function isTagValid($name) {
+    global $mtextEmptyTags;
 
-$tagStack=array();
-$xmlQueue=array();
-$i=0;
-while($i<strlen($s))
-     {
-     if($s[$i]!='<')
-       {
-       $pos=strpos($s,'<',$i);
-       if($pos===false)
-         $pos=strlen($s);
-       $xmlQueue[]=new MTText(delicateSpecialChars(substr($s,$i,$pos-$i)));
-       $i=$pos;
-       }
-     else
-       {
-       $pos=strpos($s,'>',$i);
-       if($pos===false)
-         {
-         $xmlQueue[]=new MTText(delicateSpecialChars(substr($s,$i)));
-	 $i=strlen($s);
-	 continue;
-	 }
-       $tag=substr($s,$i,$pos-$i+1);
-       if(!processTag($tag,$tagStack,$xmlQueue))
-         {
-         $xmlQueue[]=new MTText('&lt;');
-	 $i++;
-	 }
-       else
-         $i=$pos+1;
-       }
-     }
-closeTag('',$tagStack,$xmlQueue,null);
-$return=joinQueue($xmlQueue);
-return $return;
+    if ($name == '')
+        return false;
+    $name = strtoupper($name);
+    if ($name{0} == '/')
+        $name = substr($name, 1);
+    return isset($mtextEmptyTags[$name]);
 }
 
-function xmlFormatToXML($s,$format,$dformat)
-{
-$c=$s;
-if($dformat>=MTEXT_SHORT)
-  $c=replaceParagraphs($c);
-return delicateAmps(cleanupXML($c));
+function joinQueue(array $queue) {
+    while (count($queue) > 0) {
+        if ($queue[0] instanceof MTTag && $queue[0]->getProperName() == 'BR') {
+            array_shift($queue);
+        } elseif (count($queue) >= 2
+                  && $queue[0] instanceof MTTag
+                  && $queue[0]->getName() == 'P'
+                  && $queue[1] instanceof MTTag
+                  && $queue[1]->getName()=='/P') {
+            array_shift($queue);
+            array_shift($queue);
+        } else {
+            break;
+        }
+    }
+    while (count($queue) > 0) {
+        if ($queue[count($queue) - 1] instanceof MTTag
+            && $queue[count($queue) - 1]->getProperName() == 'BR') {
+            array_pop($queue);
+        } elseif (count($queue) >= 2
+                  && $queue[count($queue) - 1] instanceof MTTag
+                  && $queue[count($queue) - 1]->getName() == '/P'
+                  && $queue[count($queue) - 2] instanceof MTTag
+                  && $queue[count($queue) - 2]->getName() == 'P') {
+            array_pop($queue);
+            array_pop($queue);
+        } else {
+            break;
+        }
+    }
+    $out = '';
+    foreach($queue as $obj)
+        $out .= $obj->toString();
+    return $out;
+}
+
+function closeTag($tagName, array &$tagStack, array &$xmlQueue,
+                  MTTag $tagObject) {
+    $rstack = array();
+    while (count($tagStack) > 0
+           && $tagStack[count($tagStack) - 1] != $tagName) {
+        $tag = array_pop($tagStack);
+        $xmlQueue[] = new MTTag($tag, '</'.strtolower($tag).'>');
+        $rstack[] = $tag;
+    }
+    if ($tagName == '')
+        return;
+    if (count($tagStack) == 0)
+        $xmlQueue[] = new MTTag($tagName, '<'.strtolower($tagName).'>');
+    else
+        array_pop($tagStack);
+    $xmlQueue[] = $tagObject;
+    while (count($rstack) > 0) {
+        $tag = array_pop($rstack);
+        $xmlQueue[] = new MTTag($tag, '<'.strtolower($tag).'>');
+        $tagStack[] = $tag;
+    }
+}
+
+function processTag($tag, array &$tagStack, array &$xmlQueue) {
+    global $mtextEmptyTags;
+
+    preg_match('/^<\s*(\/?\w+)\s*(.*)>$/', $tag, $m);
+    if (!isset($m[1]) || !isTagValid($m[1]))
+        return false;
+    $out = '<'.$m[1];
+    $tagName = strtoupper($m[1]);
+    $tail = isset($m[2]) ? $m[2] : '';
+    debugLog(LL_DEBUG, 'processing tag: tagName=(%) tail=(%)',
+             array($tagName, $tail));
+    while ($tail != '') {
+        if (preg_match('/^([-\w]+)\s*=\s*("[^"]*"|\'[^\']*\')\s*(.*)$/',
+                       $tail, $m)) {
+            $out .= ' '.$m[1].'='.$m[2];
+            $tail = isset($m[3]) ? $m[3] : '';
+        } elseif (preg_match('/^([-\w]+)\s*=\s*(\S+)\s*(.*)$/', $tail, $m)) {
+            $out .= ' '.$m[1].'="'.$m[2].'"';
+            $tail = isset($m[3]) ? $m[3] : '';
+        } elseif (preg_match('/^([-\w]+)\s*(.*)$/', $tail, $m)) {
+            $out .= ' '.$m[1].'="'.$m[1].'"';
+            $tail = isset($m[2]) ? $m[2] : '';
+        } else {
+            $tail = substr($tail, 1);
+        }
+    }
+    $tagObject = new MTTag($tagName, $out);
+    if ($tagName[0] == '/') {
+        if (!$mtextEmptyTags[substr($tagName, 1)]) {
+            $tagObject->appendText('>');
+            closeTag(substr($tagName, 1), $tagStack, $xmlQueue, $tagObject);
+        }
+        // For empty tags closing tag is silently ignored
+    } else {
+        if (!$mtextEmptyTags[$tagName]) {
+            $tagObject->appendText('>');
+            $tagStack[] = $tagName;
+            $xmlQueue[] = $tagObject;
+        } else {
+            $tagObject->appendText(' />');
+            $xmlQueue[] = $tagObject;
+        }
+    }
+    return true;
+}
+
+function cleanupXML($s) {
+    global $mtextEmptyTags;
+
+    $tagStack = array();
+    $xmlQueue = array();
+    $i = 0;
+    while ($i < strlen($s)) {
+        if ($s[$i] != '<') {
+            $pos = strpos($s, '<', $i);
+            if ($pos === false)
+                $pos = strlen($s);
+            $xmlQueue[] = new MTText(delicateSpecialChars(
+                substr($s, $i, $pos - $i)
+            ));
+            $i = $pos;
+        } else {
+            $pos = strpos($s, '>', $i);
+            if ($pos === false) {
+                $xmlQueue[] = new MTText(delicateSpecialChars(substr($s, $i)));
+                $i = strlen($s);
+                continue;
+            }
+            $tag = substr($s, $i, $pos - $i + 1);
+            if (!processTag($tag, $tagStack, $xmlQueue)) {
+                $xmlQueue[] = new MTText('&lt;');
+                $i++;
+            } else {
+                $i = $pos + 1;
+            }
+        }
+    }
+    closeTag('', $tagStack, $xmlQueue, null);
+    $return = joinQueue($xmlQueue);
+    return $return;
+}
+
+function xmlFormatToXML($s, $format, $dformat) {
+    $c = $s;
+    if ($dformat >= MTEXT_SHORT)
+        $c = replaceParagraphs($c);
+    return delicateAmps(cleanupXML($c));
 }
 ?>
