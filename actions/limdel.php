0381cb464b5cb07f08e88a387fb1d442bf251136
Shmuel Melamud <balu@migdal.ru>
2014-06-26 21:24:07 +0000

    Formatted limdel.php

--- actions/limdel.php
+++ actions/limdel.php
@@ -11,16 +11,15 @@ require_once('lib/modbits.php');
 require_once('lib/sql.php');
 require_once('lib/errors.php');
 
-function deleteImageAction($image)
-{
-if($image->getId()==0 || !$image->isWritable())
-  return ELID_NO_EDIT;
-$posting=getPostingById($image->up);
-if(!$posting->isAppendable())
-  return ELID_POSTING_APPEND;
-deleteImage($image->getId());
-setPremoderates($posting,$posting);
-return EG_OK;
+function deleteImageAction($image) {
+    if ($image->getId() == 0 || !$image->isWritable())
+        return ELID_NO_EDIT;
+    $posting = getPostingById($image->up);
+    if (!$posting->isAppendable())
+        return ELID_POSTING_APPEND;
+    deleteImage($image->getId());
+    setPremoderates($posting, $posting);
+    return EG_OK;
 }
 
 httpRequestString('okdir');
@@ -30,15 +29,25 @@ httpRequestInteger('editid');
 
 dbOpen();
 session();
-$image=getImageById($editid);
-$err=deleteImageAction($image);
-if($err==EG_OK)
-  header('Location: '.remakeURI($okdir,
-                                array('err'),
-				array('editid' => 0)));
+$image = getImageById($editid);
+$err = deleteImageAction($image);
+if ($err == EG_OK)
+    header(
+        'Location: '.
+        remakeURI(
+            $okdir,
+            array('err'),
+            array('editid' => 0)
+        )
+    );
 else
-  header('Location: '.remakeURI($faildir,
-                                array(),
-				array('err' => $err)).'#error');
+    header(
+        'Location: '.
+        remakeURI(
+            $faildir,
+            array(),
+            array('err' => $err)
+        ).'#error'
+    );
 dbClose();
 ?>
