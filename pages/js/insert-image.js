d1d6abc10aa9ebdbd89dba9b96027e9d42388e21
Shmuel Melamud <balu@migdal.ru>
2014-06-30 23:15:23 +0000

    Showing insert image div on mouse move.

---
+++ pages/js/insert-image.js
@@ -0,0 +1,37 @@
+function insertImageMouseMoved(event) {
+    var div = null;
+    var minDiff = 100;
+    $(".article P").each(function() {
+        var diff = Math.abs(event.pageY - $(this).offset().top);
+        if (diff <= 25 && diff < minDiff) {
+            var dv = $(this).prev();
+            if (dv.hasClass("insert-image")) {
+                div = dv;
+                minDiff = diff;
+            }
+        }
+    });
+    if (div == window.currentInsertImage) {
+        return;
+    }
+    if (window.currentInsertImage != null) {
+        window.currentInsertImage.css("visibility", "hidden");
+    }
+    if (div != null) {
+        div.css("visibility", "visible");
+    }
+    window.currentInsertImage = div;
+}
+
+function insertImageMouseOut() {
+    if (window.currentInsertImage != null) {
+        window.currentInsertImage.css("visibility", "hidden");
+        window.currentInsertImage = null;
+    }
+}
+
+$(function() {
+    window.currentInsertImage = null;
+    $(".article").mousemove(insertImageMouseMoved)
+                 .mouseout(insertImageMouseOut);;
+});
