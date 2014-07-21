863e43190c40895b7f39aa875de6b87796543cc3
Shmuel Melamud <balu@migdal.ru>
2014-07-21 22:47:58 +0000

    Formatting

--- actions/modbits.php
+++ actions/modbits.php
@@ -11,22 +11,21 @@ require_once('lib/random.php');
 require_once('lib/entries.php');
 require_once('lib/modbits.php');
 
-function modifyEntry($id,$modbits)
-{
-global $userModerator;
+function modifyEntry($id, array $modbits) {
+    global $userModerator;
 
-if(!$userModerator)
-  return EMO_NO_MODERATE;
-if(!entryExists(ENT_NULL,$id))
-  return EMO_NO_ENTRY;
-setHiddenByEntryId($id,in_array(MOD_HIDDEN,$modbits) ? 1 : 0);
-setDisabledByEntryId($id,in_array(MOD_DISABLED,$modbits) ? 1 : 0);
-$bits=0;
-foreach($modbits as $bit)
-       if($bit>MOD_NONE && $bit<MOD_ALL)
-         $bits|=$bit;
-assignModbitsByEntryId($id,$bits);
-return EG_OK;
+    if (!$userModerator)
+        return EMO_NO_MODERATE;
+    if (!entryExists(ENT_NULL, $id))
+        return EMO_NO_ENTRY;
+    setHiddenByEntryId($id, in_array(MOD_HIDDEN, $modbits) ? 1 : 0);
+    setDisabledByEntryId($id, in_array(MOD_DISABLED, $modbits) ? 1 : 0);
+    $bits = 0;
+    foreach ($modbits as $bit)
+        if ($bit > MOD_NONE && $bit < MOD_ALL)
+            $bits |= $bit;
+    assignModbitsByEntryId($id, $bits);
+    return EG_OK;
 }
 
 httpRequestString('okdir');
@@ -37,13 +36,23 @@ httpRequestIntegerArray('modbits');
 
 dbOpen();
 session();
-$err=modifyEntry($id,$modbits);
-if($err==EG_OK)
-  header('Location: '.remakeURI($okdir,
-				array('err')));
+$err = modifyEntry($id, $modbits);
+if ($err == EG_OK)
+    header(
+        'Location: '.
+        remakeURI(
+            $okdir,
+            array('err')
+        )
+    );
 else
-  header('Location: '.remakeURI($faildir,
-				array(),
-				array('err' => $err)).'#error');
+    header(
+        'Location: '.
+        remakeURI(
+            $faildir,
+            array(),
+            array('err' => $err)
+        ).'#error'
+    );
 dbClose();
 ?>
