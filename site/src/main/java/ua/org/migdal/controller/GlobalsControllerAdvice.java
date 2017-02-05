a3daa8fc22071a8042151ee585ff36f31741edb2
Shmuel Melamud <smelamud@redhat.com>
2017-02-05 22:37:50 +0200

    Migrated <preamble-tiny> completely.

--- site/src/main/java/ua/org/migdal/controller/GlobalsControllerAdvice.java
+++ site/src/main/java/ua/org/migdal/controller/GlobalsControllerAdvice.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import ua.org.migdal.Config;
 import ua.org.migdal.Session;
 import ua.org.migdal.util.Utils;
 
@@ -18,15 +19,39 @@ public class GlobalsControllerAdvice {
     @Autowired
     private Session session;
 
+    @Autowired
+    private Config config;
+
+    @Autowired
+    private Utils utils;
+
     @ModelAttribute
     public void session(HttpServletRequest request, Model model) {
         model.addAttribute("session", session);
         model.addAttribute("location", Utils.createLocalBuilderFromRequest(request).toUriString());
+        model.addAttribute("printLocation",
+                Utils.createLocalBuilderFromRequest(request).queryParam("print", 1).toUriString());
+        model.addAttribute("print", "1".equals(request.getParameter("print")));
+        String subdomain = getUserDomain(request);
+        model.addAttribute("siteDomain", config.getSiteDomain());
+        model.addAttribute("userDomain", subdomain);
+        model.addAttribute("wwwDomain", subdomain.equals("www"));
+        model.addAttribute("englishDomain", subdomain.equals("english"));
+        model.addAttribute("siteName", !subdomain.equals("english") ? "Мигдаль" : "Migdal");
+        model.addAttribute("pageTitle", "");
+        model.addAttribute("metaNoIndex", false);
+        model.addAttribute("rssHref", "");
+        model.addAttribute("now", System.currentTimeMillis());
 
         Map<String, Object> props = new HashMap<>();
         props.put("signIn", true);
         props.put("signUp", true);
         model.addAttribute("menu", props);
+    }
+
+    private String getUserDomain(HttpServletRequest request) {
+        String hostname = Utils.createBuilderFromRequest(request).build().getHost();
+        return utils.validateSubdomain(hostname).getSubdomain();
     }
 
 }
