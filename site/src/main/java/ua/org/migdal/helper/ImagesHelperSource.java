9116543b4c6f7debd0c48d7ac534e473a27ee430
Shmuel Melamud <smelamud@redhat.com>
2017-02-06 10:12:33 +0200

    Automatically insert static image dimensions into <img> tag.

--- site/src/main/java/ua/org/migdal/helper/ImagesHelperSource.java
+++ site/src/main/java/ua/org/migdal/helper/ImagesHelperSource.java
@@ -1,17 +1,41 @@
 package ua.org.migdal.helper;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.util.HtmlUtils;
+
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Options;
-import org.springframework.web.util.HtmlUtils;
 
 @HelperSource
 public class ImagesHelperSource {
 
+    private static Logger log = LoggerFactory.getLogger(ImagesHelperSource.class);
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    private Map<String, Dimension> imageSizeCache = new HashMap<>();
+
     public CharSequence image(String href, Options options) {
         StringBuilder buf = new StringBuilder();
-        buf.append("<img src=\"");
-        buf.append(href);
-        buf.append('"');
+        buf.append("<img");
+        appendAttribute(buf, "src", href);
+        Dimension imageSize = getImageSize(href);
+        if (imageSize != null) {
+            appendAttribute(buf, "width", imageSize.width);
+            appendAttribute(buf, "height", imageSize.height);
+        }
         if (!options.<Boolean>get("englishDomain") || options.hash("alt_en") == null) {
             appendHashParam(buf, "alt", options);
         } else {
@@ -32,12 +56,34 @@ public class ImagesHelperSource {
     }
 
     private void appendHashParam(StringBuilder buf, String name, String attrName, Options options) {
-        if (options.hash(name) != null) {
+        appendAttribute(buf, attrName, options.hash(name));
+    }
+
+    private void appendAttribute(StringBuilder buf, String attributeName, Object value) {
+        if (value != null) {
             buf.append(' ');
-            buf.append(attrName);
+            buf.append(attributeName);
             buf.append("=\"");
-            buf.append(HtmlUtils.htmlEscape(options.hash(name)));
+            buf.append(HtmlUtils.htmlEscape(value.toString()));
             buf.append('"');
+        }
+    }
+
+    private Dimension getImageSize(String path) {
+        Dimension imageSize = imageSizeCache.get(path);
+        if (imageSize != null) {
+            return imageSize;
+        }
+
+        try {
+            BufferedImage image =
+                    ImageIO.read(applicationContext.getResource("classpath:static" + path).getInputStream());
+            imageSize = new Dimension(image.getWidth(), image.getHeight());
+            imageSizeCache.put(path, imageSize);
+            return imageSize;
+        } catch (IOException e) {
+            log.error("Error determining size of the image '{}': {}", path, e.getMessage());
+            return null;
         }
     }
 
