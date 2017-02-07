abef343108ea05726f7c2d3c2023a38153168a01
Shmuel Melamud <smelamud@redhat.com>
2017-02-07 09:58:07 +0200

    Fixed type DataTimeHelperSource -> DateTimeHelperSource.

--- site/src/main/java/ua/org/migdal/helper/DateTimeHelperSource.java
+++ site/src/main/java/ua/org/migdal/helper/DateTimeHelperSource.java
@@ -1,7 +1,7 @@
 package ua.org.migdal.helper;
 
 @HelperSource
-public class DataTimeHelperSource {
+public class DateTimeHelperSource {
 
     public CharSequence now() {
         return Long.toString(System.currentTimeMillis());
