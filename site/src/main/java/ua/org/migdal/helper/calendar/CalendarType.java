c1f994a4c1e63aff01071b6268b46427e2d4310f
Shmuel Melamud <smelamud@redhat.com>
2017-02-07 13:40:33 +0200

    Tables for Russian Gregorian and English/Russian Jewish calendar.

---
+++ site/src/main/java/ua/org/migdal/helper/calendar/CalendarType.java
@@ -0,0 +1,16 @@
+package ua.org.migdal.helper.calendar;
+
+public enum CalendarType {
+
+    GREGORIAN_EN,
+    GREGORIAN_RU_NOM_PC,
+    GREGORIAN_RU_GEN_PC,
+    GREGORIAN_RU_NOM_LC,
+    GREGORIAN_RU_GEN_LC,
+    JEWISH_EN,
+    JEWISH_RU_NOM_PC,
+    JEWISH_RU_GEN_PC,
+    JEWISH_RU_NOM_LC,
+    JEWISH_RU_GEN_LC
+
+}
---
+++ site/src/main/java/ua/org/migdal/helper/calendar/Tables.java
@@ -0,0 +1,57 @@
+package ua.org.migdal.helper.calendar;
+
+public class Tables {
+    
+    public static String[] GREGORIAN_DOW_RU_PC_SHORT3 = {"Пнд", "Втр", "Срд", "Чтв", "Птн", "Суб", "Вск"};
+    public static String[] GREGORIAN_DOW_RU_LC_SHORT3 = {"пнд", "втр", "срд", "чтв", "птн", "суб", "вск"};
+    public static String[] GREGORIAN_DOW_RU_PC_SHORT2 = {"Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"};
+    public static String[] GREGORIAN_DOW_RU_LC_SHORT2 = {"пн", "вт", "ср", "чт", "пт", "сб", "вс"};
+    public static String[] GREGORIAN_DOW_RU_PC_LONG = {"Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг",
+            "Пятница", "Суббота"};
+    public static String[] GREGORIAN_DOW_RU_LC_LONG = {"воскресенье", "понедельник", "вторник", "среда", "четверг",
+            "пятница", "суббота"};
+
+    public static String[] GREGORIAN_MONTH_RU_PC_SHORT = {"Янв", "Фев", "Мар", "Апр", "Май", "Июн",
+            "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"};
+    public static String[] GREGORIAN_MONTH_RU_LC_SHORT = {"янв", "фев", "мар", "апр", "май", "июн",
+            "июл", "авг", "сен", "окт", "ноя", "дек"};
+    public static String[] GREGORIAN_MONTH_RU_NOM_PC_LONG = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+            "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+    public static String[] GREGORIAN_MONTH_RU_NOM_LC_LONG = {"январь", "февраль", "март", "апрель", "май", "июнь",
+            "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"};
+    public static String[] GREGORIAN_MONTH_RU_GEN_PC_LONG = {"Января", "Февраля", "Марта", "Апреля", "Мая", "Июня",
+            "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"};
+    public static String[] GREGORIAN_MONTH_RU_GEN_LC_LONG = {"января", "февраля", "марта", "апреля", "мая", "июня",
+            "июля", "августа", "сентября", "октября", "ноября", "декабря"};
+
+    public static String[] JEWISH_DOW_RU_PC_SHORT = {"Ршн", "Шен", "Шлш", "Рви", "Хмш", "Шиш", "Шаб"};
+    public static String[] JEWISH_DOW_RU_LC_SHORT = {"ршн", "шен", "шлш", "рви", "хмш", "шиш", "шаб"};
+    public static String[] JEWISH_DOW_RU_PC_LONG = {"Йом-ришон", "Йом-шейни", "Йом-шлиши", "Йом-рвии", "Йом-хамиши",
+            "Йом-шиши", "Шабат"};
+    public static String[] JEWISH_DOW_RU_LC_LONG = {"йом-ришон", "йом-шейни", "йом-шлиши", "йом-рвии", "йом-хамиши",
+            "йом-шиши", "шабат"};
+
+    public static String[] JEWISH_MONTH_RU_PC_SHORT = {"Тиш", "Хеш", "Кис", "Тев", "Швт", "Ад1",
+            "Ад2", "Нис", "Ияр", "Сив", "Там", "Ав", "Элл", "Адр"};
+    public static String[] JEWISH_MONTH_RU_LC_SHORT = {"тиш", "хеш", "кис", "тев", "швт", "ад1",
+            "ад2", "нис", "ияр", "сив", "там", "ав", "элл", "адр"};
+    public static String[] JEWISH_MONTH_RU_NOM_PC_LONG = {"Тишрей", "Хешван", "Кислев", "Тевет", "Шват", "Адар I",
+            "Адар II", "Нисан", "Ияр", "Сиван", "Тамуз", "Ав", "Элул", "Адар"};
+    public static String[] JEWISH_MONTH_RU_NOM_LC_LONG = {"тишрей", "хешван", "кислев", "тевет", "шват", "адар I",
+            "адар II", "нисан", "ияр", "сиван", "тамуз", "ав", "элул", "адар"};
+    public static String[] JEWISH_MONTH_RU_GEN_PC_LONG = {"Тишрей", "Хешвана", "Кислева", "Тевета", "Швата", "Адара I",
+            "Адара II", "Нисана", "Ияра", "Сивана", "Тамуза", "Ава", "Элула", "Адара"};
+    public static String[] JEWISH_MONTH_RU_GEN_LC_LONG = {"тишрей", "хешвана", "кислева", "тевета", "швата", "адара I",
+            "адара II", "нисана", "ияра", "сивана", "тамуза", "ава", "элула", "адара"};
+    public static short[] JEWISH_MONTH_LENGTH = {30, 29, 30, 29, 30, 30, 29, 30, 29, 30, 29, 30, 29};
+
+    public static String[] JEWISH_DOW_EN_SHORT = {"Rsh", "Shn", "Shl", "Rvi", "Hms", "Shs", "Shb"};
+    public static String[] JEWISH_DOW_EN_LONG = {"Yom-rishon", "Yom-sheini", "Yom-shlishi", "Yom-rvii", "Yom-hamishi",
+            "Yom-shishi", "Shabbat"};
+
+    public static String[] JEWISH_MONTH_EN_SHORT = {"Tis", "Hes", "Kis", "Tev", "Shv", "Ad1",
+            "Ad2", "Nis", "Iyr", "Siv", "Tam", "Av", "Ell", "Adr"};
+    public static String[] JEWISH_MONTH_EN_LONG = {"Tishrej", "Heshvan", "Kislev", "Tevet", "Shvat", "Adar I",
+            "Adar II", "Nisan", "Iyar", "Sivan", "Tammuz", "Av", "Elul", "Adar"};
+
+}
