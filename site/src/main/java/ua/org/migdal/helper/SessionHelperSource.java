01433c4f8bd7e646524e82eda280355112c96e4e
Shmuel Melamud <smelamud@redhat.com>
2017-02-12 00:17:27 +0200

    Migrated <bottom>.

--- site/src/main/java/ua/org/migdal/helper/SessionHelperSource.java
+++ site/src/main/java/ua/org/migdal/helper/SessionHelperSource.java
@@ -12,6 +12,14 @@ public class SessionHelperSource {
     @Autowired
     private RequestContext requestContext;
 
+    public CharSequence print(Options options) throws IOException {
+        return options.<Boolean>get("print") ? options.apply(options.fn) : options.apply(options.inverse);
+    }
+
+    public CharSequence notPrint(Options options) throws IOException {
+        return !options.<Boolean>get("print") ? options.apply(options.fn) : options.apply(options.inverse);
+    }
+
     public CharSequence english(Options options) throws IOException {
         return options.get("userDomain").equals("english")
                 ? options.apply(options.fn)
@@ -30,6 +38,10 @@ public class SessionHelperSource {
 
     public CharSequence notLogged(Options options) throws IOException {
         return !requestContext.isLogged() ? options.apply(options.fn) : options.apply(options.inverse);
+    }
+
+    public CharSequence moderator(Options options) throws IOException {
+        return requestContext.isUserModerator() ? options.apply(options.fn) : options.apply(options.inverse);
     }
 
     public CharSequence adminUsers(Options options) throws IOException {
