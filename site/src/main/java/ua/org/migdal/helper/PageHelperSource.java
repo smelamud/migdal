a3d73719e88c577e865b91a226cc174e365c7858
Shmuel Melamud <smelamud@redhat.com>
2017-02-28 02:52:54 +0200

    Safe version of PageHelperSource.

--- site/src/main/java/ua/org/migdal/helper/PageHelperSource.java
+++ site/src/main/java/ua/org/migdal/helper/PageHelperSource.java
@@ -2,15 +2,14 @@ package ua.org.migdal.helper;
 
 import com.github.jknack.handlebars.Handlebars.SafeString;
 import com.github.jknack.handlebars.Options;
-import org.springframework.web.util.HtmlUtils;
 
 @HelperSource
 public class PageHelperSource {
 
     public CharSequence subtitle(Options options) {
-        String pageTitle = HtmlUtils.htmlEscape(options.get("pageTitle"));
+        String pageTitle = options.get("pageTitle");
         if (pageTitle != null && !pageTitle.isEmpty() && !pageTitle.equals("Главная") && !pageTitle.equals("Home")) {
-            return new SafeString(String.format("<div class=\"page-title\">%s</div>", pageTitle));
+            return new SafeString(String.format("<div class=\"page-title\">%s</div>", HelperUtils.he(pageTitle)));
         } else {
             return "";
         }
