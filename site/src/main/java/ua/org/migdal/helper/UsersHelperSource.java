6057b234b05ea215e7135cf22459e3dc4cb51c04
Shmuel Melamud <smelamud@redhat.com>
2017-02-28 02:46:48 +0200

    Safe version of UsersHelperSource.

--- site/src/main/java/ua/org/migdal/helper/UsersHelperSource.java
+++ site/src/main/java/ua/org/migdal/helper/UsersHelperSource.java
@@ -1,7 +1,5 @@
 package ua.org.migdal.helper;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.github.jknack.handlebars.Handlebars.SafeString;
 
 import ua.org.migdal.data.User;
@@ -10,18 +8,15 @@ import ua.org.migdal.helper.calendar.Formatter;
 @HelperSource
 public class UsersHelperSource {
 
-    @Autowired
-    private StringHelperSource stringHelperSource;
-
     public CharSequence mailToLink(User user) {
         if (!user.isEmailVisible()) {
             return "";
         }
         StringBuilder buf = new StringBuilder();
         buf.append("<a href=\"mailto:");
-        buf.append(stringHelperSource.ue(user.getEmail()));
+        buf.append(HelperUtils.ue(user.getEmail()));
         buf.append("\" title=\"Написать письмо\">");
-        buf.append(stringHelperSource.he(user.getEmail()));
+        HelperUtils.safeAppend(buf, user.getEmail());
         buf.append("</a>");
         return new SafeString(buf);
     }
