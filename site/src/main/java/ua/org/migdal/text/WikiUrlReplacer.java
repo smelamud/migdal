971effbc5d9631bd5eb3078b6c720a4d3e5ffdf0
Shmuel Melamud <smelamud@redhat.com>
2017-04-05 01:14:21 +0300

    Enum for WikiUrlReplacer states, instead of plain numbers.

--- site/src/main/java/ua/org/migdal/text/WikiUrlReplacer.java
+++ site/src/main/java/ua/org/migdal/text/WikiUrlReplacer.java
@@ -15,10 +15,18 @@ class WikiUrlReplacer {
     private static final Pattern EMAIL = Pattern.compile(
             "[A-Za-z0-9-_]+(\\.[A-Za-z0-9-_]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*");
 
+    private enum State {
+        REGULAR,
+        QUOTE,
+        OPEN_QUOTE,
+        CLOSE_QUOTE,
+        LABELED_URL
+    }
+
     private String text;
     private int start;
     private int end;
-    private int state;
+    private State state;
     private StringBuilder out = new StringBuilder();
 
     WikiUrlReplacer(String text) {
@@ -38,7 +46,7 @@ class WikiUrlReplacer {
                 content);
     }
 
-    private void goFurther(int targetState) {
+    private void goFurther(State targetState) {
         if (end > start) {
             out.append(Utils.replaceAll(text.substring(start, end), URL_DELIMITED,
                     (m) -> m.group(1) + getUrlTag(m.group(2), m.group(2), m.group(3), m.group(2))));
@@ -48,47 +56,48 @@ class WikiUrlReplacer {
     }
 
     private String replace() {
+        state = State.REGULAR;
         Matcher m = null;
         while (end < text.length()) {
             switch (state) {
-                case 0:
+                case REGULAR:
                     end = text.indexOf('\'', start);
                     end = end < 0 ? text.length() : end;
-                    goFurther(1);
+                    goFurther(State.QUOTE);
                     break;
 
-                case 1:
+                case QUOTE:
                     end = start + 1;
                     if (start != 0 && !WikiText.isDelimiter(text.charAt(start - 1))) {
-                        goFurther(0);
+                        goFurther(State.REGULAR);
                     } else {
-                        state = 2;
+                        state = State.OPEN_QUOTE;
                     }
                     break;
 
-                case 2:
+                case OPEN_QUOTE:
                     end = text.indexOf('\'', end);
                     end = end < 0 ? text.length() : end + 1;
-                    state = 3;
+                    state = State.CLOSE_QUOTE;
                     break;
 
-                case 3:
+                case CLOSE_QUOTE:
                     m = URL_FOLLOWING.matcher(text.substring(end));
                     if (!m.lookingAt()) {
                         end--;
-                        goFurther(0);
+                        goFurther(State.REGULAR);
                     } else {
-                        state = 4;
+                        state = State.LABELED_URL;
                     }
                     break;
 
-                case 4:
+                case LABELED_URL:
                     if (m != null) { // opposite never happens, we check only to avoid compilation error
                         out.append(getUrlTag(m.group(0), m.group(1), m.group(2), text.substring(start + 1, end - 1)));
                         start = end + m.group(0).length();
                     }
                     end = start;
-                    state = 0;
+                    state = State.REGULAR;
                     break;
             }
         }
