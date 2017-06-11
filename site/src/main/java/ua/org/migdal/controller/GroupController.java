d2141785f267aea1f71a8d6cf9f7f91701e5ff36
Shmuel Melamud <smelamud@redhat.com>
2017-06-11 16:34:55 +0300

    Starting to migrate grouporg.mhtml.

---
+++ site/src/main/java/ua/org/migdal/controller/GroupController.java
@@ -0,0 +1,31 @@
+package ua.org.migdal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ua.org.migdal.session.LocationInfo;
+
+@Controller
+public class GroupController {
+
+    @Autowired
+    private IndexController indexController;
+
+    @GetMapping("/admin/groups")
+    public String adminGroups(Model model) {
+        adminGroupsLocationInfo(model);
+
+        return "admin-groups";
+    }
+
+    public LocationInfo adminGroupsLocationInfo(Model model) {
+        return new LocationInfo(model)
+                .withUri("/admin/groups")
+                .withTopics("topics-admin")
+                .withTopicsIndex("admin-groups")
+                .withParent(indexController.indexLocationInfo(null))
+                .withPageTitle("Группы");
+    }
+
+}
