4c7bf961d5c71879cc8cbb3d11150de10448e43f
Shmuel Melamud <smelamud@redhat.com>
2017-07-07 00:04:58 +0300

    Needless methods in EntryManager.

--- site/src/main/java/ua/org/migdal/manager/EntryManager.java
+++ site/src/main/java/ua/org/migdal/manager/EntryManager.java
@@ -5,8 +5,6 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
 
 import ua.org.migdal.data.EntryRepository;
@@ -26,17 +24,6 @@ public class EntryManager {
     @Cacheable("entries-track")
     public String getTrackById(long id) {
         return entryRepository.findTrackById(id);
-    }
-
-    @Query("select e.modbits from Entry e where id=?1")
-    public String getModbitsById(long id) {
-        return entryRepository.findModbitsById(id);
-    }
-
-    @Modifying
-    @Query("update Entry e set e.modbits=?2 where id=?1")
-    public String updateModbitsById(long id) {
-        return entryRepository.updateModbitsById(id);
     }
 
 }
