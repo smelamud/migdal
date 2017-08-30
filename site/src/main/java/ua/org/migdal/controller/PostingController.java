bd5462dcdaab0e20a031b531556fa05159a58387
Shmuel Melamud <smelamud@redhat.com>
2017-08-31 01:48:31 +0300

    Topic choice in admin of postings.

--- site/src/main/java/ua/org/migdal/controller/PostingController.java
+++ site/src/main/java/ua/org/migdal/controller/PostingController.java
@@ -6,18 +6,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import ua.org.migdal.form.AdminPostingsForm;
+import ua.org.migdal.manager.TopicManager;
 import ua.org.migdal.session.LocationInfo;
 
 @Controller
 public class PostingController {
 
     @Inject
+    private TopicManager topicManager;
+
+    @Inject
     private IndexController indexController;
 
     @GetMapping("/admin/postings")
-    public String adminPostings(Model model) {
+    public String adminPostings(@ModelAttribute AdminPostingsForm adminPostingsForm, Model model) {
         adminPostingsLocationInfo(model);
 
+        model.addAttribute("topicNames", topicManager.begNames(0, -1, false, false));
+        model.addAttribute("adminPostingsForm", adminPostingsForm);
         return "admin-postings";
     }
 
---
+++ site/src/main/java/ua/org/migdal/form/AdminPostingsForm.java
@@ -0,0 +1,89 @@
+package ua.org.migdal.form;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AdminPostingsForm implements Serializable {
+
+    private static final long serialVersionUID = 6852165340260083018L;
+
+    public static class TopicChoice {
+
+        private int n;
+        private long topicId;
+        private boolean recursive;
+
+        public TopicChoice(int n, long topicId, boolean recursive) {
+            this.n = n;
+            this.topicId = topicId;
+            this.recursive = recursive;
+        }
+
+        public int getN() {
+            return n;
+        }
+
+        public void setN(int n) {
+            this.n = n;
+        }
+
+        public long getTopicId() {
+            return topicId;
+        }
+
+        public void setTopicId(long topicId) {
+            this.topicId = topicId;
+        }
+
+        public boolean isRecursive() {
+            return recursive;
+        }
+
+        public void setRecursive(boolean recursive) {
+            this.recursive = recursive;
+        }
+
+    }
+
+    private long[] topicIds = new long[0];
+    private int[] recursive = new int[0];
+
+    public AdminPostingsForm() {
+    }
+
+    public long[] getTopicIds() {
+        return topicIds;
+    }
+
+    public void setTopicIds(long[] topicIds) {
+        this.topicIds = topicIds;
+    }
+
+    public int[] getRecursive() {
+        return recursive;
+    }
+
+    public void setRecursive(int[] recursive) {
+        this.recursive = recursive;
+        Arrays.sort(this.recursive);
+    }
+
+    public List<TopicChoice> getTopicChoices() {
+        List<TopicChoice> choices = new ArrayList<>();
+
+        if (topicIds != null) {
+            for (int i = 0; i < topicIds.length; i++) {
+                boolean rcv = Arrays.binarySearch(recursive, i) >= 0;
+                choices.add(new TopicChoice(i, topicIds[i], rcv));
+            }
+        }
+        if (topicIds == null || topicIds.length == 0 || topicIds[topicIds.length - 1] > 0) {
+            choices.add(new TopicChoice(topicIds != null ? topicIds.length : 0, 0, true));
+        }
+
+        return choices;
+    }
+
+}
