86590770dd67cdd4e8d60ca0905c0d34f7b81f4a
Shmuel Melamud <smelamud@redhat.com>
2017-09-04 17:22:37 +0300

    Beginning to migrate the posting editor.

--- site/src/main/java/ua/org/migdal/controller/PostingController.java
+++ site/src/main/java/ua/org/migdal/controller/PostingController.java
@@ -3,17 +3,19 @@ package ua.org.migdal.controller;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import ua.org.migdal.data.Posting;
 import ua.org.migdal.data.Topic;
 import ua.org.migdal.form.AdminPostingsForm;
+import ua.org.migdal.form.TopicForm;
 import ua.org.migdal.manager.PostingManager;
 import ua.org.migdal.manager.TopicManager;
 import ua.org.migdal.session.LocationInfo;
@@ -77,6 +79,23 @@ public class PostingController {
                 .withTopicsIndex("admin-postings")
                 .withParent(indexController.indexLocationInfo(null))
                 .withPageTitle("Сообщения");
+    }
+
+    @GetMapping("/admin/postings/add")
+    private String postingAdd(@RequestParam boolean full, Model model) {
+        postingAddLocationInfo(model);
+
+        model.addAttribute("topicNames", topicManager.begNames(0, -1, false, true));
+        model.addAttribute("full", full);
+        model.asMap().computeIfAbsent("postingForm", key -> new TopicForm());
+        return "posting-edit";
+    }
+
+    public LocationInfo postingAddLocationInfo(Model model) {
+        return new LocationInfo(model)
+                .withUri("/admin/postings/add")
+                .withParent(adminPostingsLocationInfo(null))
+                .withPageTitle("Добавление сообщения");
     }
 
 }
---
+++ site/src/main/java/ua/org/migdal/form/PostingForm.java
@@ -0,0 +1,9 @@
+package ua.org.migdal.form;
+
+import java.io.Serializable;
+
+public class PostingForm implements Serializable {
+
+    private static final long serialVersionUID = 4588207664747142717L;
+
+}
