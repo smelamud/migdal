d86ac56338fbfac89858ca410e2b974e9cfbd01a
Shmuel Melamud <smelamud@redhat.com>
2017-10-29 00:41:37 +0300

    Do not use @RequestMapping.

--- site/src/main/java/ua/org/migdal/controller/TopicController.java
+++ site/src/main/java/ua/org/migdal/controller/TopicController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -280,8 +279,8 @@ public class TopicController {
                 .withPageTitle("Удаление темы");
     }
 
-    // FIXME need to allow only POST requests
-    @RequestMapping("/actions/topic/delete")
+    @GetMapping("/actions/topic/delete") // FIXME need to allow only POST requests
+    @PostMapping("/actions/topic/delete")
     public String actionTopicDelete(
             @ModelAttribute @Valid TopicDeleteForm topicDeleteForm,
             Errors errors,
