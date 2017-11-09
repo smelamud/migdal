18338ee3b178d6d992e236fcc7df73968d096b04
Shmuel Melamud <smelamud@redhat.com>
2017-11-09 21:17:32 +0200

    Base of /admin/ears.

---
+++ site/src/main/java/ua/org/migdal/controller/EarController.java
@@ -0,0 +1,60 @@
+package ua.org.migdal.controller;
+
+import java.util.Collections;
+
+import javax.inject.Inject;
+
+import org.springframework.data.util.Pair;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ua.org.migdal.grp.GrpEnum;
+import ua.org.migdal.manager.IdentManager;
+import ua.org.migdal.manager.PostingManager;
+import ua.org.migdal.session.LocationInfo;
+
+@Controller
+public class EarController {
+
+    @Inject
+    private PostingManager postingManager;
+
+    @Inject
+    private IdentManager identManager;
+
+    @Inject
+    private GrpEnum grpEnum;
+
+    @Inject
+    private AdminController adminController;
+
+    @GetMapping("/admin/ears")
+    public String adminPostings(
+            @RequestParam(defaultValue = "0") Integer offset,
+            Model model) {
+        adminEarsLocationInfo(model);
+
+        long topicId = identManager.getIdByIdent("ears");
+        model.addAttribute("postings",
+                        postingManager.begAll(
+                                Collections.singletonList(Pair.of(topicId, true)),
+                                grpEnum.group("EARS"),
+                                null,
+                                null,
+                                offset,
+                                20));
+        return "admin-ears";
+    }
+
+    public LocationInfo adminEarsLocationInfo(Model model) {
+        return new LocationInfo(model)
+                .withUri("/admin/ears")
+                .withTopics("topics-admin")
+                .withTopicsIndex("admin-ears")
+                .withParent(adminController.adminLocationInfo(null))
+                .withPageTitle("Ушки");
+    }
+
+}
