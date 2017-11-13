8cd0f148830f97507a47a29acc84ec332dd447db
Shmuel Melamud <smelamud@redhat.com>
2017-11-13 23:24:28 +0200

    Helpers for checking entry permissions.

--- site/src/main/java/ua/org/migdal/helper/SessionHelperSource.java
+++ site/src/main/java/ua/org/migdal/helper/SessionHelperSource.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import com.github.jknack.handlebars.Options;
 
 import ua.org.migdal.data.Editable;
+import ua.org.migdal.data.Entry;
 import ua.org.migdal.session.RequestContext;
 
 @HelperSource
@@ -49,6 +50,30 @@ public class SessionHelperSource {
 
     public CharSequence editable(Editable object, Options options) throws IOException {
         return object != null && object.isEditable(requestContext)
+                ? options.apply(options.fn)
+                : options.apply(options.inverse);
+    }
+
+    public CharSequence readable(Entry entry, Options options) throws IOException {
+        return entry != null && entry.isReadable()
+                ? options.apply(options.fn)
+                : options.apply(options.inverse);
+    }
+
+    public CharSequence writable(Entry entry, Options options) throws IOException {
+        return entry != null && entry.isWritable()
+                ? options.apply(options.fn)
+                : options.apply(options.inverse);
+    }
+
+    public CharSequence appendable(Entry entry, Options options) throws IOException {
+        return entry != null && entry.isAppendable()
+                ? options.apply(options.fn)
+                : options.apply(options.inverse);
+    }
+
+    public CharSequence postable(Entry entry, Options options) throws IOException {
+        return entry != null && entry.isPostable()
                 ? options.apply(options.fn)
                 : options.apply(options.inverse);
     }
