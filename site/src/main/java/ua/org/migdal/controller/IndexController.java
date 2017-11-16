68cab10ae8656fdba0013500856845f218b9411f
Shmuel Melamud <smelamud@redhat.com>
2017-11-16 17:03:25 +0200

    tzdaka.mhtml migrated.

--- site/src/main/java/ua/org/migdal/controller/IndexController.java
+++ site/src/main/java/ua/org/migdal/controller/IndexController.java
@@ -6,21 +6,33 @@ import javax.inject.Inject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import ua.org.migdal.controller.exception.PageNotFoundException;
 import ua.org.migdal.data.CrossEntry;
 import ua.org.migdal.data.LinkType;
+import ua.org.migdal.data.Topic;
 import ua.org.migdal.manager.CrossEntryManager;
+import ua.org.migdal.manager.IdentManager;
+import ua.org.migdal.manager.TopicManager;
 import ua.org.migdal.session.LocationInfo;
 
 @Controller
 public class IndexController {
 
     @Inject
+    private IdentManager identManager;
+
+    @Inject
     private CrossEntryManager crossEntryManager;
+
+    @Inject
+    private TopicManager topicManager;
 
     @GetMapping("/")
     public String index(Model model) {
         indexLocationInfo(model);
 
+        model.addAttribute("topic", null);
         addMajors(model);
         return "index-www";
     }
@@ -33,6 +45,33 @@ public class IndexController {
                 .withTopics("topics-major")
                 .withTopicsIndex("index")
                 .withPageTitle("Главная");
+    }
+
+    @GetMapping("/{ident}")
+    public String major(@PathVariable String ident, Model model) throws PageNotFoundException {
+        long id = identManager.idOrIdent(ident);
+        if (id <= 0) {
+            throw new PageNotFoundException();
+        }
+        Topic topic = topicManager.beg(id);
+        if (topic == null) {
+            throw new PageNotFoundException();
+        }
+
+        majorLocationInfo(topic, model);
+
+        model.addAttribute("topic", topic);
+        addMajors(model);
+        return "index-www";
+    }
+
+    public LocationInfo majorLocationInfo(Topic topic, Model model) {
+        return new LocationInfo(model)
+                .withUri("/" + topic.getIdent())
+                .withRssHref("/rss/")
+                .withTopics("topics-major")
+                .withTopicsIndex(topic.getIdent())
+                .withPageTitle(topic.getSubject());
     }
 
     private void addMajors(Model model) {
--- site/src/main/java/ua/org/migdal/helper/PageHelperSource.java
+++ site/src/main/java/ua/org/migdal/helper/PageHelperSource.java
@@ -1,11 +1,16 @@
 package ua.org.migdal.helper;
 
+import javax.inject.Inject;
+
 import com.github.jknack.handlebars.Handlebars.SafeString;
 import com.github.jknack.handlebars.Options;
 import ua.org.migdal.helper.util.HelperUtils;
 
 @HelperSource
 public class PageHelperSource {
+
+    @Inject
+    private ImagesHelperSource imagesHelperSource;
 
     public CharSequence subtitle(Options options) {
         String pageTitle = options.get("pageTitle");
@@ -14,6 +19,15 @@ public class PageHelperSource {
         } else {
             return "";
         }
+    }
+
+    public CharSequence tzdaka(Options options) {
+        StringBuilder buf = new StringBuilder();
+        buf.append("<a href=\"/migdal/tzdaka/\">");
+        String title = "Наши реквизиты для цдаки";
+        buf.append(imagesHelperSource.image("/pics/tzedaka.jpg", title, title));
+        buf.append("</a>");
+        return new SafeString(buf);
     }
 
 }
