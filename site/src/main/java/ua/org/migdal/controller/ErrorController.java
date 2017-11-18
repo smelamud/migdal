e597458268a787432009f7442a89cc07e3bc3f9a
Shmuel Melamud <smelamud@redhat.com>
2017-11-18 22:49:45 +0200

    Special pages for 404 and 500 error codes only, default for others.

--- site/src/main/java/ua/org/migdal/controller/ErrorController.java
+++ site/src/main/java/ua/org/migdal/controller/ErrorController.java
@@ -1,11 +1,13 @@
 package ua.org.migdal.controller;
 
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.boot.autoconfigure.web.ErrorProperties;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,8 +19,16 @@ public class ErrorController extends BasicErrorController {
     }
 
     @Override
-    public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
-        return new ModelAndView("404");
+    protected ModelAndView resolveErrorView(HttpServletRequest request, HttpServletResponse response,
+                                            HttpStatus status, Map<String, Object> model) {
+        switch (status) {
+            case NOT_FOUND:
+                return new ModelAndView("404", model);
+            case INTERNAL_SERVER_ERROR:
+                return new ModelAndView("500", model);
+            default:
+                return super.resolveErrorView(request, response, status, model);
+        }
     }
 
 }
