e8d94ec06304864250a3bd646758f623233fb200
Shmuel Melamud <smelamud@redhat.com>
2018-06-27 01:55:28 +0300

    Selecting text for quoting.

--- site/src/main/js/reply.js
+++ site/src/main/js/reply.js
@@ -8,22 +8,25 @@ function userLink(userName, guestName) {
 function replyInit() {
     $("a.forum-reply").click(function(event) {
         var comment = $(this).parent().parent().parent();
-        var body = comment.find(".body").html().trim();
         var userName = comment.find(".header .name").html();
         var guestName = comment.find(".header .guest-name").html();
 
-        body = body.replace(/[\r\n]/g, "")
-                   .replace(/<a href="mailto:[^"]+">([^<]+)<\/a>/g, "$1")
-                   .replace(/<a class="name"[^>]*>([^<]+)<\/a>/g, "<user name=\"$1\">")
-                   .replace(/<span class="guest-name">([^<]+)<\/span>/g, "<user guest-name=\"$1\">")
-                   .replace(/<div class="quote">/g, "<quote>")
-                   .replace(/<\/p>\s*<\/div>/g, "</quote>")
-                   .replace(/<\/div>/g, "</quote>")
-                   .replace(/<p>/g, "")
-                   .replace(/<\/p>$/, "")
-                   .replace(/<\/p>/g, "\n\n")
-                   .replace(/<br>/g, "\n")
-                   .replace(/^\n+/, "");
+        var body = window.getSelection().toString();
+        if (!body) {
+            body = comment.find(".body").html().trim();
+            body = body.replace(/[\r\n]/g, "")
+                       .replace(/<a href="mailto:[^"]+">([^<]+)<\/a>/g, "$1")
+                       .replace(/<a class="name"[^>]*>([^<]+)<\/a>/g, "<user name=\"$1\">")
+                       .replace(/<span class="guest-name">([^<]+)<\/span>/g, "<user guest-name=\"$1\">")
+                       .replace(/<div class="quote">/g, "<quote>")
+                       .replace(/<\/p>\s*<\/div>/g, "</quote>")
+                       .replace(/<\/div>/g, "</quote>")
+                       .replace(/<p>/g, "")
+                       .replace(/<\/p>$/, "")
+                       .replace(/<\/p>/g, "\n\n")
+                       .replace(/<br>/g, "\n")
+                       .replace(/^\n+/, "");
+        }
         body = "<quote>" + userLink(userName, guestName) + " пишет:\n\n" + body + "</quote>";
 
         var textarea = $("#comment-add");
