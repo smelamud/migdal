bedc362d9744decfd09df47a7cb85379bb294bfb
Shmuel Melamud <smelamud@redhat.com>
2018-06-27 01:29:48 +0300

    User name before quoting.

--- site/src/main/js/reply.js
+++ site/src/main/js/reply.js
@@ -1,6 +1,17 @@
+function userLink(userName, guestName) {
+    if (userName) {
+        return "<user name=\"" + userName + "\">";
+    }
+    return "<user guest-name=\"" + guestName + "\">";
+}
+
 function replyInit() {
     $("a.forum-reply").click(function(event) {
-        var body = $(this).parent().parent().children(".body").html().trim();
+        var comment = $(this).parent().parent().parent();
+        var body = comment.find(".body").html().trim();
+        var userName = comment.find(".header .name").html();
+        var guestName = comment.find(".header .guest-name").html();
+
         body = body.replace(/<a href="mailto:[^"]+">([^<]+)<\/a>/g, "$1")
                    .replace(/<a class="name"[^>]*>([^<]+)<\/a>/g, "<user name=\"$1\">")
                    .replace(/<span class="guest-name">([^<]+)<\/span>/g, "<user guest-name=\"$1\">")
@@ -11,12 +22,14 @@ function replyInit() {
                    .replace(/<\/p>$/, "")
                    .replace(/<\/p>/g, "\n\n")
                    .replace(/<br>/g, "\n");
-        body = "<quote>" + body + "</quote>";
+        body = "<quote>" + userLink(userName, guestName) + " пишет:\n\n" + body + "</quote>";
+
         var textarea = $("#comment-add");
         textarea.val(body + "\n\n" + textarea.val());
         textarea.focus();
         textarea.prop("selectionStart", body.length + 2);
         textarea.prop("selectionEnd", body.length + 2);
+
         event.preventDefault();
     })
 }
