6995f35f2c486c17e5bd2b5eae743631ebd3af97
Shmuel Melamud <smelamud@redhat.com>
2018-07-11 01:42:07 +0300

    Repository and Manager for InnerImage. FetchType for relations.

--- site/src/main/java/ua/org/migdal/data/InnerImage.java
+++ site/src/main/java/ua/org/migdal/data/InnerImage.java
@@ -3,6 +3,7 @@ package ua.org.migdal.data;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
@@ -18,7 +19,7 @@ public class InnerImage {
     @Access(AccessType.PROPERTY)
     private long id;
 
-    @ManyToOne
+    @ManyToOne(fetch=FetchType.LAZY)
     private Entry entry;
 
     @NotNull
@@ -30,7 +31,7 @@ public class InnerImage {
     @NotNull
     private int y;
 
-    @ManyToOne
+    @ManyToOne(fetch=FetchType.EAGER)
     private Image image;
 
     @NotNull
---
+++ site/src/main/java/ua/org/migdal/manager/InnerImageManager.java
@@ -0,0 +1,7 @@
+package ua.org.migdal.manager;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InnerImageManager {
+}
