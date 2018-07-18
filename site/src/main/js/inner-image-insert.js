0125319ff604ee91d3505dfc1a88cff4e59fd2f8
Shmuel Melamud <smelamud@redhat.com>
2018-07-18 22:20:36 +0300

    JS for insert-inner-image.hbs.html.

---
+++ site/src/main/js/inner-image-insert.js
@@ -0,0 +1,8 @@
+$(function() {
+    $("#thumbnail-x").keypress(function() {
+        $("#thumbnail-yes").click();
+    })
+    $("#thumbnail-y").keypress(function() {
+        $("#thumbnail-yes").click();
+    })
+});
