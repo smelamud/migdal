289ca042f7c3b1dc982537e8c89fdd6205c4e767
Shmuel Melamud <smelamud@redhat.com>
2018-11-07 16:30:27 +0200

    Made IntegerRangeIterator private inner class of IntegerRange.

--- site/src/main/java/ua/org/migdal/data/util/IntegerRange.java
+++ site/src/main/java/ua/org/migdal/data/util/IntegerRange.java
@@ -1,8 +1,38 @@
 package ua.org.migdal.data.util;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class IntegerRange implements Iterable<Long> {
+
+    private static class IntegerRangeIterator implements Iterator<Long> {
+
+        private long end;
+        private long current;
+        private long step;
+
+        IntegerRangeIterator(long begin, long end) {
+            this.end = end;
+            current = begin;
+            step = begin <= end ? 1 : -1;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return step > 0 ? current < end : current > end;
+        }
+
+        @Override
+        public Long next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            long result = current;
+            current += step;
+            return result;
+        }
+
+    }
 
     private Iterator<Long> iterator;
 
