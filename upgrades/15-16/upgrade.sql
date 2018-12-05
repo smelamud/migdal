278dfd6f7fba6f6497b972311434605897ddda83
Shmuel Melamud <smelamud@redhat.com>
2018-12-05 21:58:44 +0200

    Starting cleanup.

--- upgrades/15-16/upgrade.sql
+++
@@ -1,2 +0,0 @@
-UPDATE entries SET rating = vote -3 * cast( vote_count AS signed ) ;
-UPDATE `version` SET `db_version` = '16' WHERE `version`.`db_version` =15 LIMIT 1 ;
--- upgrades/18-19/upgrade7.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO entry_grps( entry_id, grp ) 
-SELECT entries.id, 4096
-FROM  `entries` 
-LEFT JOIN entry_grps ON entry_grps.entry_id = entries.id
-WHERE  `entry` =3
-AND entry_grps.grp =4;
