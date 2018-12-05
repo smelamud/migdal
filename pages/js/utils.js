61e391cf6443a93da64805436514d2f9182b5b69
Shmuel Melamud <smelamud@redhat.com>
2018-12-05 22:45:50 +0200

    Cleanup mailings/, pages/

--- pages/js/utils.js
+++
@@ -1,9 +0,0 @@
-// @(#) $Id$
-
-function plural(n, forms) {
-    var a = n % 10;
-    var b = Math.floor(n / 10) % 10;
-    return b == 1 || a >= 5 || a == 0
-           ? forms[2]
-           : (a == 1 ? forms[0] : forms[1]);
-}
