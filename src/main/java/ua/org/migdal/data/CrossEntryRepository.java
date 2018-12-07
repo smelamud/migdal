85dc13c7320762b0d21383ecef7d19c209824d06
Shmuel Melamud <smelamud@redhat.com>
2018-12-07 10:46:39 +0200

    Force joins when querying CrossEntry objects.

--- src/main/java/ua/org/migdal/data/CrossEntryRepository.java
+++ src/main/java/ua/org/migdal/data/CrossEntryRepository.java
@@ -3,13 +3,18 @@ package ua.org.migdal.data;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CrossEntryRepository extends JpaRepository<CrossEntry, Long> {
 
     CrossEntry findByLinkTypeAndPeerId(LinkType linkType, long peerId);
 
+    // We need @Query here to force join
+    @Query("from CrossEntry ce left join ce.peer where ce.linkType=?1 and ce.sourceName=?2 order by ce.peer.subject")
     List<CrossEntry> findAllByLinkTypeAndSourceNameOrderByPeerSubject(LinkType linkType, String sourceName);
 
+    // We need @Query here to force join
+    @Query("from CrossEntry ce left join ce.peer where ce.linkType=?1 and ce.source.id=?2 order by ce.peer.subject")
     List<CrossEntry> findAllByLinkTypeAndSourceIdOrderByPeerSubject(LinkType linkType, long sourceId);
 
 }
